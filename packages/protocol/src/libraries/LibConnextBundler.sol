b28201eb55147ab37428731a796b34994be6b046
Boyan Barakov <boyan.barakov@gmail.com>
2022-08-11 11:10:48 +0200

    feat: create LibConnextBundler

---
+++ packages/protocol/src/libraries/LibConnextBundler.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.9;
+
+/**
+ * @title Helper library for bundling operations thru Connext.
+ * @author Fujidao Labs
+ * @notice Helper library to construct params to be passed to Connext Router
+ * for the most common operations.
+ */
+
+import {IRouter} from "../interfaces/IRouter.sol";
+
+library LibConnextBundler {
+  function bridgeDepositAndBorrow(
+    uint256 destDomain,
+    address destVault,
+    address asset,
+    uint256 amount,
+    uint256 borrowAmount
+  )
+    public
+    view
+    returns (IRouter.Action[] memory actions, bytes[] memory args)
+  {
+    IRouter.Action[] memory bActions = new IRouter.Action[](2);
+    bytes[] memory bArgs = new bytes[](2);
+
+    bActions[0] = IRouter.Action.Deposit;
+    bArgs[0] = abi.encode(amount, msg.sender);
+
+    bActions[1] = IRouter.Action.Borrow;
+    bArgs[1] = abi.encode(borrowAmount, msg.sender, msg.sender);
+
+    bytes memory params = abi.encode(destVault, asset, amount, actions, args);
+
+    bytes4 selector = bytes4(keccak256("onXCall(bytes)"));
+
+    bytes memory callData = abi.encodeWithSelector(selector, params);
+
+    actions[0] = IRouter.Action.XTransferWithCall;
+    args[0] = abi.encode(destDomain, asset, amount, callData);
+  }
+}
