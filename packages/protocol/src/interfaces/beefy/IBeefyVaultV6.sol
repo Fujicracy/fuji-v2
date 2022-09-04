77fe9d51e966819c3b02855db78277771b2373bd
Boyan Barakov <boyan.barakov@gmail.com>
2022-09-04 09:49:45 +0200

    feat(protocol): yield vault of Beefy on Optimism (first try)

---
+++ packages/protocol/src/interfaces/beefy/IBeefyVaultV6.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.15;
+
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+interface IBeefyVaultV6 is IERC20 {
+  function deposit(uint256 amount) external;
+
+  function withdraw(uint256 shares) external;
+
+  function want() external pure returns (address);
+}
