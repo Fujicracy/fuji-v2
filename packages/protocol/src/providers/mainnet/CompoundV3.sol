88d115372ed3da70c9d9a4a9e81e78097d0799db
DCota <32775237+DaigaroCota@users.noreply.github.com>
2022-09-12 11:02:57 -0400

    chore(protocol): create folder mainnet provider compoundV3

---
+++ packages/protocol/src/providers/mainnet/CompoundV3.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.15;
+
+import {ILendingProvider} from "../../interfaces/ILendingProvider.sol";
+
+/**
+ * @title CompoundV3 Lending Provider.
+ * @author Fujidao Labs
+ * @notice This contract allows interaction with CompoundV3.
+ */
+contract CompoundV3 is ILendingProvider {
+
+}
