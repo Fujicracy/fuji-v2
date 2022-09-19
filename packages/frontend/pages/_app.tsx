146adf2d16a7076d1a8920f512d977b3efc8fda5
Chevalier <chevalierchris34@gmail.com>
2022-09-19 16:47:53 +0200

    feat(frontend) setup Material UI theme

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -1,11 +1,13 @@
-import "../styles/globals.css"
-import type { AppProps } from "next/app"
-import { inspect } from "@xstate/inspect"
-import { createContext, useEffect } from "react"
-import { useInterpret } from "@xstate/react"
-import { authMachine } from "../machines/auth.machine"
-import { InterpreterFrom } from "xstate"
-import mixpanel from "mixpanel-browser"
+import '../styles/globals.css'
+import { AppProps } from 'next/app'
+import { inspect } from '@xstate/inspect'
+import { createContext, useEffect } from 'react'
+import { useInterpret } from '@xstate/react'
+import { authMachine } from '../machines/auth.machine'
+import { InterpreterFrom } from 'xstate'
+import mixpanel from 'mixpanel-browser'
+import theme from '../styles/theme'
+import { ThemeProvider } from '@mui/material'
 
 // if (typeof window !== "undefined") {
 //   inspect({
@@ -19,20 +21,22 @@ interface GlobalStateContext {
 }
 export const GlobalStateContext = createContext({} as GlobalStateContext)
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp ({ Component, pageProps }: AppProps) {
   const authService = useInterpret(authMachine, {
-    devTools: process.env.NODE_ENV === "development",
+    devTools: process.env.NODE_ENV === 'development'
   })
   useEffect(() => {
-    mixpanel.init("030ddddf19623797be516b634956d108", {
-      debug: process.env.NODE_ENV === "development",
+    mixpanel.init('030ddddf19623797be516b634956d108', {
+      debug: process.env.NODE_ENV === 'development'
     })
-    authService.send("INITIALIZE")
+    authService.send('INITIALIZE')
   }, [authService])
 
   return (
     <GlobalStateContext.Provider value={{ authService }}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </GlobalStateContext.Provider>
   )
 }
---
+++ packages/frontend/styles/theme.ts
@@ -0,0 +1,125 @@
+import { createTheme } from '@mui/material'
+
+declare module '@mui/material/Button' {
+  interface ButtonPropsVariantOverrides {
+    flat: true
+    gradient: true
+  }
+}
+
+let colorTheme = createTheme({
+  palette: {
+    primary: {
+      light: '#E2E8F0',
+      main: '#222429',
+      dark: '#191B1F'
+    },
+    secondary: {
+      light: '#EC2668',
+      main: '#FE3477',
+      dark: '#F0014F'
+    },
+    success: {
+      main: '#50FE34'
+    },
+    warning: {
+      main: '#F5AC37'
+    }
+  }
+})
+
+const theme = createTheme(colorTheme, {
+  typography: {
+    fontFamily: 'Inter'
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          boxSizing: 'border-box',
+          color: colorTheme.palette.primary.light,
+          borderRadius: '8px',
+          padding: '12px 20px',
+          textTransform: 'none',
+          fontStyle: 'normal',
+          fontWeight: 500,
+          fontSize: '16px',
+          lineHeight: '19px'
+        },
+        startIcon: {},
+        disabled: {
+          opacity: 0.5
+        }
+      },
+      variants: [
+        {
+          props: { variant: 'flat' },
+          style: {
+            background:
+              'linear-gradient(92.29deg, rgba(254, 52, 119, 0.8) 0%, rgba(240, 1, 79, 0.8) 100%)',
+            boxShadow: '0px 1px 2px rgba(16, 24, 40, 0.05)'
+          }
+        },
+        {
+          props: { variant: 'gradient' },
+          style: {
+            background: `linear-gradient(287.45deg, rgba(254, 52, 119, 0) 6.81%, ${colorTheme.palette.secondary.dark} 120.29%)`,
+            border: `1px solid ${colorTheme.palette.secondary.main}`
+          }
+        }
+      ]
+    },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          background: '#191B1F',
+          borderRadius: '12px',
+          color: colorTheme.palette.primary.light
+        }
+      },
+      variants: [
+        {
+          props: { variant: 'outlined' },
+          style: {
+            background: colorTheme.palette.primary.main,
+            border: '1px solid #3A3A3C',
+            borderRadius: '8px'
+          }
+        }
+      ]
+    },
+    MuiInputBase: {
+      styleOverrides: {
+        root: {
+          fontSize: '20px',
+          lineHeight: '150%',
+          color: colorTheme.palette.primary.light
+        }
+      }
+    },
+    MuiSelect: {
+      styleOverrides: {
+        select: {
+          borderWidth: 0,
+          fontWeight: 400,
+          fontSize: '14px',
+          lineHeight: '150%',
+          color: colorTheme.palette.primary.light
+        }
+      }
+    },
+    MuiFormLabel: {
+      styleOverrides: {
+        root: {
+          fontWeight: 400,
+          fontSize: '14px',
+          lineHeight: '150%',
+          color: colorTheme.palette.primary.light,
+          opacity: 0.5
+        }
+      }
+    }
+  }
+})
+
+export default theme
