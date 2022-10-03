ebb422a6473199ebe7cf940f4bc96b08ccc7c81f
DCota <32775237+DaigaroCota@users.noreply.github.com>
2022-10-03 16:24:54 -0400

    chore(protocol): execute chief functions after deploy

--- packages/protocol/deploy/networks/mainnet/00_deployFactories.ts
+++ packages/protocol/deploy/networks/mainnet/00_deployFactories.ts
@@ -1,5 +1,5 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { DeployFunction, Address } from 'hardhat-deploy/types';
+import { DeployFunction, Deployment } from 'hardhat-deploy/types';
 
 import deployChief from "../../tasks/deployChief";
 import deployBorrowingVaultFactory from "../../tasks/deployBorrowingVaultFactory";
@@ -7,9 +7,23 @@ import deployAddrMapperFactory from "../../tasks/deployAddrMapperFactory";
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   await deployChief(hre);
-  const chief: Address = (await hre.deployments.get('Chief')).address;
-  await deployBorrowingVaultFactory(hre, chief);
+  const chiefDeployment: Deployment = (await hre.deployments.get('Chief'));
+  await deployBorrowingVaultFactory(hre, chiefDeployment.address);
+  const bVaultFactoryDeployment: Deployment = await hre.deployments.get('BorrowingVaultFactory');
   await deployAddrMapperFactory(hre);
+
+  const { deployments, getNamedAccounts } = hre;
+  const { deployer } = await getNamedAccounts();
+
+  await deployments.execute('Chief', {
+    from: deployer,
+    log: true,
+    autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
+    waitConfirmations: 1
+  },
+    'addToAllowed',
+    bVaultFactoryDeployment.address
+  );
 };
 
 export default func;
