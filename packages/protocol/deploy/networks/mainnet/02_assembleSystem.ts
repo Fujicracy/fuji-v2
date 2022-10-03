4d3f05f2fefa977a4cf74afdc6ee650cc0de8159
DCota <32775237+DaigaroCota@users.noreply.github.com>
2022-10-03 17:24:55 -0400

    chore(protocol): troubleshoot pending deploying vault

--- packages/protocol/deploy/networks/mainnet/02_assembleSystem.ts
+++ packages/protocol/deploy/networks/mainnet/02_assembleSystem.ts
@@ -1,16 +1,31 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { DeployFunction, Address } from 'hardhat-deploy/types';
+import { DeployFunction, Address, Deployment } from 'hardhat-deploy/types';
 import { ASSETS } from '../../utils/assets';
 
 import deployFujiOracle,
 { getAssetAddresses, getPriceFeedAddresses } from '../../tasks/deployFujiOracle';
 import deploySimpleRouter from '../../tasks/deploySimpleRouter';
+import { deployBorrowingVault } from '../../tasks/deployBorrowingVaultFactory';
 
 const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const assets: Address[] = getAssetAddresses('mainnet');
   const priceFeeds: Address[] = getPriceFeedAddresses('mainnet');
   await deployFujiOracle(hre, assets, priceFeeds);
   await deploySimpleRouter(hre, ASSETS['mainnet'].WETH.address);
+
+  const { deployments } = hre;
+
+  // Define vaults
+  await deployBorrowingVault(
+    hre,
+    ASSETS['mainnet'].WETH.address,
+    ASSETS['mainnet'].USDC.address,
+    (await deployments.get('FujiOracle')).address,
+    [
+      (await deployments.get('AaveV2')).address,
+      (await deployments.get('CompoundV3')).address,
+    ]
+  )
 };
 
 export default func;
--- packages/protocol/deploy/tasks/deployBorrowingVaultFactory.ts
+++ packages/protocol/deploy/tasks/deployBorrowingVaultFactory.ts
@@ -1,10 +1,10 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { Address } from 'hardhat-deploy/types';
+import { ethers } from 'hardhat';
+import { Deployment, Address } from 'hardhat-deploy/types';
 
 const deployBorrowingVaultFactory = async (hre: HardhatRuntimeEnvironment, chief: Address) => {
   const { deployments, getNamedAccounts } = hre;
   const { deploy } = deployments;
-
   const { deployer } = await getNamedAccounts();
 
   await deploy('BorrowingVaultFactory', {
@@ -16,6 +16,33 @@ const deployBorrowingVaultFactory = async (hre: HardhatRuntimeEnvironment, chief
     waitConfirmations: 1
   });
 };
+
+export const deployBorrowingVault = async (
+  hre: HardhatRuntimeEnvironment,
+  asset: Address,
+  debtAsset: Address,
+  oracle: Address,
+  providers: Address[]
+) => {
+  const { deployments, getNamedAccounts } = hre;
+  const { deployer } = await getNamedAccounts();
+  const abiCoder = ethers.utils.defaultAbiCoder;
+
+  const bVaultFactoryDeployment: Deployment = await hre.deployments.get('BorrowingVaultFactory');
+  const vaultData = abiCoder.encode(["address", "address", "address"], [asset, debtAsset, oracle]);
+
+  await deployments.execute('Chief', {
+    from: deployer,
+    log: true,
+    autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
+    waitConfirmations: 1
+  },
+    'deployVault',
+    bVaultFactoryDeployment.address,
+    vaultData
+  );
+
+}
 
 export default deployBorrowingVaultFactory;
 deployBorrowingVaultFactory.tags = ['BorrowingVaultFactory'];
