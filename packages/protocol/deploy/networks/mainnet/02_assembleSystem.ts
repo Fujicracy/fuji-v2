27387c878f888b252cd193dba09e52a9b936fac0
DCota <32775237+DaigaroCota@users.noreply.github.com>
2022-10-04 16:47:07 -0400

    chore(protocol): remove vault deployment from deploy scripts

--- packages/protocol/deploy/networks/mainnet/02_assembleSystem.ts
+++ packages/protocol/deploy/networks/mainnet/02_assembleSystem.ts
@@ -13,20 +13,6 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 
   await deployFujiOracle(hre, assets, priceFeeds);
   await deploySimpleRouter(hre, ASSETS['mainnet'].WETH.address);
-
-  const { deployments } = hre;
-
-  // Define vaults
-  await deployBorrowingVault(
-    hre,
-    ASSETS['mainnet'].WETH.address,
-    ASSETS['mainnet'].USDC.address,
-    (await deployments.get('FujiOracle')).address,
-    [
-      (await deployments.get('AaveV2')).address,
-      (await deployments.get('CompoundV3')).address,
-    ]
-  )
 };
 
 export default func;
