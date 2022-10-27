c98ea7570a3dab0acf47ec8b2c879189b16df1cb
Boyan Barakov <boyan.barakov@gmail.com>
2022-10-27 18:32:32 +0200

    test(protocol): introduce new test structure

---
+++ packages/protocol/test/forking/ForkingSetup.sol
@@ -0,0 +1,178 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.15;
+
+import {Test} from "forge-std/Test.sol";
+import {TimelockController} from
+  "openzeppelin-contracts/contracts/governance/TimelockController.sol";
+import {LibSigUtils} from "../../src/libraries/LibSigUtils.sol";
+import {BorrowingVault} from "../../src/vaults/borrowing/BorrowingVault.sol";
+import {MockOracle} from "../../src/mocks/MockOracle.sol";
+import {MockERC20} from "../../src/mocks/MockERC20.sol";
+import {Chief} from "../../src/Chief.sol";
+import {IVault} from "../../src/interfaces/IVault.sol";
+import {IVaultPermissions} from "../../src/interfaces/IVaultPermissions.sol";
+import {ILendingProvider} from "../../src/interfaces/ILendingProvider.sol";
+import {CoreRoles} from "../../src/access/CoreRoles.sol";
+
+contract ForkingSetup is CoreRoles, Test {
+  uint32 public constant GOERLI_DOMAIN = 1735353714;
+  uint32 public constant OPTIMISM_GOERLI_DOMAIN = 1735356532;
+  uint32 public constant MUMBAI_DOMAIN = 9991;
+  uint32 public constant MAINNET_DOMAIN = 6648936;
+
+  uint256 public constant ALICE_PK = 0xA;
+  address public ALICE = vm.addr(ALICE_PK);
+  uint256 public constant BOB_PK = 0xB;
+  address public BOB = vm.addr(BOB_PK);
+  uint256 public constant CHARLIE_PK = 0xC;
+  address public CHARLIE = vm.addr(CHARLIE_PK);
+
+  uint32 originDomain;
+
+  struct Registry {
+    address weth;
+    address usdc;
+    address connext;
+  }
+  // domain => addresses registry
+
+  mapping(uint256 => Registry) public registry;
+  // domain => forkId
+  mapping(uint256 => uint256) public forks;
+
+  IVault public vault;
+  Chief public chief;
+  TimelockController public timelock;
+  MockOracle mockOracle;
+
+  address public collateralAsset;
+  address public debtAsset;
+
+  constructor() {
+    vm.label(ALICE, "alice");
+    vm.label(BOB, "bob");
+    vm.label(CHARLIE, "charlie");
+
+    forks[GOERLI_DOMAIN] = vm.createFork("goerli");
+    forks[OPTIMISM_GOERLI_DOMAIN] = vm.createFork("optimism_goerli");
+    forks[MUMBAI_DOMAIN] = vm.createFork("mumbai");
+    forks[MAINNET_DOMAIN] = vm.createFork("mainnet");
+
+    Registry memory goerli = Registry({
+      weth: 0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6,
+      usdc: address(0),
+      connext: 0x99A784d082476E551E5fc918ce3d849f2b8e89B6
+    });
+    registry[GOERLI_DOMAIN] = goerli;
+
+    Registry memory optimismGoerli = Registry({
+      weth: 0x74c6FD7D2Bc6a8F0Ebd7D78321A95471b8C2B806,
+      usdc: address(0),
+      connext: 0x705791AD27229dd4CCf41b6720528AfE1bcC2910
+    });
+    registry[OPTIMISM_GOERLI_DOMAIN] = optimismGoerli;
+
+    Registry memory mumbai = Registry({
+      weth: 0xFD2AB41e083c75085807c4A65C0A14FDD93d55A9,
+      usdc: address(0),
+      connext: 0xfeBBcfe9a88aadefA6e305945F2d2011493B15b4
+    });
+    registry[MUMBAI_DOMAIN] = mumbai;
+
+    Registry memory mainnet = Registry({
+      weth: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,
+      usdc: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,
+      connext: address(0)
+    });
+    registry[MAINNET_DOMAIN] = mainnet;
+  }
+
+  function deploy(uint32 domain) public {
+    Registry memory reg = registry[domain];
+    if (reg.connext == address(0) && reg.weth == address(0) && reg.usdc == address(0)) {
+      revert("No registry for this chain");
+    }
+    vm.selectFork(forks[domain]);
+
+    originDomain = domain;
+
+    if (reg.connext != address(0)) vm.label(reg.connext, "Connext");
+
+    collateralAsset = reg.weth;
+    vm.label(reg.weth, "ConnextWETH");
+
+    if (reg.usdc == address(0)) {
+      // mostly for testnets
+      MockERC20 tDAI = new MockERC20("Test DAI", "tDAI");
+      debtAsset = address(tDAI);
+      vm.label(debtAsset, "testDAI");
+    } else {
+      debtAsset = reg.usdc;
+      vm.label(debtAsset, "USDC");
+    }
+
+    // TODO: replace with real oracle
+    mockOracle = new MockOracle();
+    // WETH and DAI prices by Aug 12h 2022
+    mockOracle.setPriceOf(collateralAsset, debtAsset, 528881643782407);
+    mockOracle.setPriceOf(debtAsset, collateralAsset, 1889069940262927605990);
+
+    address[] memory admins = new address[](1);
+    admins[0] = address(this);
+    timelock = new TimelockController(1 days, admins, admins);
+
+    chief = new Chief();
+    chief.setTimelock(address(timelock));
+    // Grant this address all roles.
+    chief.grantRole(REBALANCER_ROLE, address(this));
+    chief.grantRole(LIQUIDATOR_ROLE, address(this));
+
+    vault = new BorrowingVault(
+      collateralAsset,
+      debtAsset,
+      address(mockOracle),
+      address(chief),
+      "Fuji-V2 WETH Vault Shares",
+      "fv2WETH"
+    );
+  }
+
+  function _callWithTimelock(bytes memory sendData, IVault v) internal {
+    timelock.schedule(address(v), 0, sendData, 0x00, 0x00, 1.5 days);
+    vm.warp(block.timestamp + 2 days);
+    timelock.execute(address(v), 0, sendData, 0x00, 0x00);
+    rewind(2 days);
+  }
+
+  function _setVaultProviders(IVault v, ILendingProvider[] memory providers) internal {
+    bytes memory sendData = abi.encodeWithSelector(IVault.setProviders.selector, providers);
+    _callWithTimelock(sendData, v);
+  }
+
+  // plusNonce is necessary for compound operations,
+  // those that needs more than one signiture in the same tx
+  function _getPermitBorrowArgs(
+    address owner,
+    uint256 ownerPrivateKey,
+    address operator,
+    uint256 borrowAmount,
+    uint256 plusNonce,
+    address vault_
+  ) internal returns (uint256 deadline, uint8 v, bytes32 r, bytes32 s) {
+    deadline = block.timestamp + 1 days;
+    LibSigUtils.Permit memory permit = LibSigUtils.Permit({
+      owner: owner,
+      spender: operator,
+      amount: borrowAmount,
+      nonce: IVaultPermissions(vault_).nonces(owner) + plusNonce,
+      deadline: deadline
+    });
+    bytes32 structHash = LibSigUtils.getStructHashBorrow(permit);
+    bytes32 digest = LibSigUtils.getHashTypedDataV4Digest(
+      // This domain should be obtained from the chain on which state will change.
+      IVaultPermissions(vault_).DOMAIN_SEPARATOR(),
+      structHash
+    );
+    (v, r, s) = vm.sign(ownerPrivateKey, digest);
+  }
+}
---
+++ packages/protocol/test/forking/goerli/ConnextRouter.t.sol
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.15;
+
+import "forge-std/console.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Routines} from "../../utils/Routines.sol";
+import {ForkingSetup} from "../ForkingSetup.sol";
+import {AaveV3Goerli} from "../../../src/providers/goerli/AaveV3Goerli.sol";
+import {ILendingProvider} from "../../../src/interfaces/ILendingProvider.sol";
+import {MockProvider} from "../../../src/mocks/MockProvider.sol";
+import {IRouter} from "../../../src/interfaces/IRouter.sol";
+import {IConnext} from "../../../src/interfaces/connext/IConnext.sol";
+import {BorrowingVault} from "../../../src/vaults/borrowing/BorrowingVault.sol";
+import {ConnextRouter} from "../../../src/routers/ConnextRouter.sol";
+import {IWETH9} from "../../../src/helpers/PeripheryPayments.sol";
+
+contract ConnextRouterTest is Routines, ForkingSetup {
+  event Deposit(address indexed sender, address indexed owner, uint256 assets, uint256 shares);
+
+  event Borrow(
+    address indexed sender,
+    address indexed receiver,
+    address indexed owner,
+    uint256 debt,
+    uint256 shares
+  );
+
+  event Dispatch(bytes32 leaf, uint256 index, bytes32 root, bytes message);
+
+  ConnextRouter public connextRouter;
+
+  function setUp() public {
+    deploy(GOERLI_DOMAIN);
+
+    connextRouter = new ConnextRouter(
+      IWETH9(collateralAsset),
+      IConnext(registry[GOERLI_DOMAIN].connext)
+    );
+    // addresses are supposed to be the same across different chains
+    connextRouter.setRouter(GOERLI_DOMAIN, address(connextRouter));
+    connextRouter.setRouter(OPTIMISM_GOERLI_DOMAIN, address(connextRouter));
+    connextRouter.setRouter(MUMBAI_DOMAIN, address(connextRouter));
+
+    // test with a mock provider because Connext's and Aave's WETH mismatch
+    MockProvider mockProvider = new MockProvider();
+    ILendingProvider[] memory providers = new ILendingProvider[](1);
+    providers[0] = mockProvider;
+
+    _setVaultProviders(vault, providers);
+    vault.setActiveProvider(mockProvider);
+  }
+
+  function test_bridgeOutbound() public {
+    uint256 amount = 2 ether;
+    deal(collateralAsset, ALICE, amount);
+
+    uint32 destDomain = originDomain == GOERLI_DOMAIN ? OPTIMISM_GOERLI_DOMAIN : GOERLI_DOMAIN;
+
+    vm.startPrank(ALICE);
+
+    SafeERC20.safeApprove(IERC20(collateralAsset), address(connextRouter), type(uint256).max);
+
+    IRouter.Action[] memory actions = new IRouter.Action[](1);
+    bytes[] memory args = new bytes[](1);
+
+    actions[0] = IRouter.Action.XTransferWithCall;
+    bytes memory randomData = abi.encode(keccak256("data_data"));
+    args[0] = abi.encode(destDomain, collateralAsset, amount, randomData);
+
+    /*bytes4 selector =*/
+    /*bytes4(keccak256("xCall(uint32,address,address,address,uint256,uint256,bytes)"));*/
+    /*bytes memory callData = abi.encodeWithSelector(*/
+    /*selector,*/
+    /*destDomain,*/
+    /*connextRouter.routerByDomain(destDomain),*/
+    /*collateralAsset,*/
+    /*ALICE,*/
+    /*amount,*/
+    /*30,*/
+    /*randomData*/
+    /*);*/
+
+    /*vm.expectCall(address(connext), "");*/
+
+    vm.expectEmit(false, false, false, false);
+    emit Dispatch("", 1, "", "");
+
+    connextRouter.xBundle(actions, args);
+  }
+
+  function test_bridgeInbound() public {
+    uint256 amount = 2 ether;
+    uint256 borrowAmount = 1000e6;
+
+    IRouter.Action[] memory actions = new IRouter.Action[](3);
+    actions[0] = IRouter.Action.Deposit;
+    actions[1] = IRouter.Action.PermitBorrow;
+    actions[2] = IRouter.Action.Borrow;
+
+    bytes[] memory args = new bytes[](3);
+    args[0] = abi.encode(address(vault), amount, ALICE, address(connextRouter));
+
+    (uint256 deadline, uint8 v, bytes32 r, bytes32 s) =
+      _getPermitBorrowArgs(ALICE, ALICE_PK, address(connextRouter), borrowAmount, 0, address(vault));
+
+    args[1] =
+      abi.encode(address(vault), ALICE, address(connextRouter), borrowAmount, deadline, v, r, s);
+
+    args[2] = abi.encode(address(vault), borrowAmount, ALICE, ALICE);
+
+    bytes memory callData = abi.encode(actions, args);
+
+    vm.expectEmit(true, true, true, false);
+    emit Deposit(address(connextRouter), ALICE, amount, amount);
+
+    vm.expectEmit(true, true, true, false);
+    emit Borrow(address(connextRouter), ALICE, ALICE, borrowAmount, borrowAmount);
+
+    // send directly the bridged funds to our router
+    // thus mocking Connext behavior
+    deal(collateralAsset, address(connextRouter), amount);
+    address originSender = connextRouter.routerByDomain(originDomain);
+
+    vm.startPrank(registry[GOERLI_DOMAIN].connext);
+    connextRouter.xReceive("", 0, address(0), originSender, originDomain, callData);
+
+    assertEq(vault.balanceOf(ALICE), amount);
+  }
+}
