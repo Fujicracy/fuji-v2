5bc063b0dfcef5e2711add731a94be600d356fae
pedrovalido <pedro.validex@gmail.com>
2022-11-01 09:49:07 +0000

    test(protocol) add support for arbitrum

--- packages/protocol/test/utils/DSTestPlus.sol
+++ packages/protocol/test/utils/DSTestPlus.sol
@@ -13,4 +13,5 @@ contract DSTestPlus is Test {
   uint32 public goerliChainId = 5;
   uint32 public optimismGoerliChainId = 420;
   uint32 public polygonChainId = 137;
+  uint32 public arbitrumChainId = 42161;
 }
