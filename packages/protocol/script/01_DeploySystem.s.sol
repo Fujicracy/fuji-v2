f545644045c509d68dd2a98d87848397dfe4178b
Boyan Barakov <boyan.barakov@gmail.com>
2022-11-08 16:47:05 +0100

    refactor(protocol): foundry script directory

--- packages/protocol/script/01_DeploySystem.s.sol
+++ packages/protocol/script/01_DeploySystem.s.sol
@@ -5,21 +5,22 @@ import "forge-std/console.sol";
 import {ScriptPlus} from "./ScriptPlus.sol";
 import {IConnext} from "../src/interfaces/connext/IConnext.sol";
 import {BorrowingVault} from "../src/vaults/borrowing/BorrowingVault.sol";
-import {ILendingProvider} from "../src/interfaces/ILendingProvider.sol";
 import {IVault} from "../src/interfaces/IVault.sol";
 import {Chief} from "../src/Chief.sol";
-import {IChief} from "../src/interfaces/IChief.sol";
 import {ConnextRouter} from "../src/routers/ConnextRouter.sol";
 import {IWETH9} from "../src/helpers/PeripheryPayments.sol";
 import {MockProvider} from "../src/mocks/MockProvider.sol";
 import {MockERC20} from "../src/mocks/MockERC20.sol";
 import {MockOracle} from "../src/mocks/MockOracle.sol";
+import {ILendingProvider} from "../src/interfaces/ILendingProvider.sol";
+import {IERC20Metadata} from
+  "openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
-contract DeployOptimismGoerli is ScriptPlus {
+contract DeployGoerli is ScriptPlus {
   IVault public vault;
   IWETH9 public weth;
-  IChief public chief;
   IConnext public connextHandler;
+  Chief public chief;
 
   ConnextRouter public connextRouter;
 
@@ -30,51 +31,53 @@ contract DeployOptimismGoerli is ScriptPlus {
   function setUp() public {
     chainName = "optimism-goerli";
 
-    weth = IWETH9(0x4E283927E35b7118eA546Ef58Ea60bfF59E857DB);
-    connextHandler = IConnext(0xe37f1f55eab648dA87047A03CB03DeE3d3fe7eC7);
+    weth = IWETH9(getAddress("WETH"));
+    connextHandler = IConnext(getAddress("ConnextHandler"));
   }
 
   function run() public {
     vm.startBroadcast();
 
+    /*mockDAI = MockERC20(getAddress("MockDAI"))*/
     mockDAI = new MockERC20("Test DAI", "tDAI");
     saveAddress("MockDAI", address(mockDAI));
 
+    /*mockOracle = MockOracle(getAddress("MockOracle"));*/
     mockOracle = new MockOracle();
     saveAddress("MockOracle", address(mockOracle));
 
+    /*mockProvider = MockProvider(getAddress("MockProvider"));*/
     mockProvider = new MockProvider();
     saveAddress("MockProvider", address(mockProvider));
 
+    /*chief = Chief(getAddress("Chief"));*/
     chief = new Chief();
+    saveAddress("Chief", address(chief));
+    bytes32 REBALANCER_ROLE = keccak256("REBALANCER_ROLE");
+    chief.grantRole(REBALANCER_ROLE, msg.sender);
+    chief.setTimelock(msg.sender);
 
     connextRouter = new ConnextRouter(weth, connextHandler, chief);
     saveAddress("ConnextRouter", address(connextRouter));
 
     vault = new BorrowingVault(
       address(weth),
-      getAddress("MockDAI"),
-      getAddress("MockOracle"),
-      address(0),
+      address(mockDAI),
+      address(mockOracle),
+      address(chief),
       "Fuji-V2 WETH Vault Shares",
       "fv2WETH"
     );
-    saveAddress("BorrowingVault", address(vault));
+    saveAddress("BorrowingVault-DAI", address(vault));
 
     ILendingProvider[] memory providers = new ILendingProvider[](1);
-    providers[0] = MockProvider(getAddress("MockProvider"));
-    BorrowingVault(payable(getAddress("BorrowingVault"))).setProviders(providers);
-    BorrowingVault(payable(getAddress("BorrowingVault"))).setActiveProvider(
-      MockProvider(getAddress("MockProvider"))
-    );
+    providers[0] = mockProvider;
+    vault.setProviders(providers);
+    vault.setActiveProvider(mockProvider);
 
     // WETH and DAI prices by Aug 12h 2022
-    MockOracle(getAddress("MockOracle")).setPriceOf(
-      address(weth), getAddress("MockDAI"), 528881643782407
-    );
-    MockOracle(getAddress("MockOracle")).setPriceOf(
-      getAddress("MockDAI"), address(weth), 1889069940262927605990
-    );
+    mockOracle.setPriceOf(address(weth), address(mockDAI), 528881643782407);
+    mockOracle.setPriceOf(address(mockDAI), address(weth), 1889069940);
 
     vm.stopBroadcast();
   }
--- packages/protocol/script/bash/rinkeby/aave_v3.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-. ./script/bash/rinkeby/common.sh
-
-deploy_contract AaveV3Rinkeby
