890ae504dd0320986ea2ae52456c585999659a0f
pedrovalido <pedro.validex@gmail.com>
2022-11-09 18:14:53 +0000

    fix(protocol) remove unused methods

--- packages/protocol/src/interfaces/euler/IEulerEToken.sol
+++ packages/protocol/src/interfaces/euler/IEulerEToken.sol
@@ -3,48 +3,12 @@ pragma solidity 0.8.15;
 
 /// @notice Tokenised representation of assets
 interface IEulerEToken {
-    /// @notice Pool name, ie "Euler Pool: DAI"
-    function name() external view returns (string memory);
-
-    /// @notice Pool symbol, ie "eDAI"
-    function symbol() external view returns (string memory);
-
-    /// @notice Decimals, always normalised to 18.
-    function decimals() external pure returns (uint8);
 
     /// @notice Address of underlying asset
     function underlyingAsset() external view returns (address);
 
-    /// @notice Sum of all balances, in internal book-keeping units (non-increasing)
-    function totalSupply() external view returns (uint);
-
-    /// @notice Sum of all balances, in underlying units (increases as interest is earned)
-    function totalSupplyUnderlying() external view returns (uint);
-
     /// @notice Balance of a particular account, in internal book-keeping units (non-increasing)
     function balanceOf(address account) external view returns (uint);
-
-    /// @notice Balance of a particular account, in underlying units (increases as interest is earned)
-    function balanceOfUnderlying(address account) external view returns (uint);
-
-    /// @notice Balance of the reserves, in internal book-keeping units (non-increasing)
-    function reserveBalance() external view returns (uint);
-
-    /// @notice Balance of the reserves, in underlying units (increases as interest is earned)
-    function reserveBalanceUnderlying() external view returns (uint);
-
-    /// @notice Convert an eToken balance to an underlying amount, taking into account current exchange rate
-    /// @param balance eToken balance, in internal book-keeping units (18 decimals)
-    /// @return Amount in underlying units, (same decimals as underlying token)
-    function convertBalanceToUnderlying(uint balance) external view returns (uint);
-
-    /// @notice Convert an underlying amount to an eToken balance, taking into account current exchange rate
-    /// @param underlyingAmount Amount in underlying units (same decimals as underlying token)
-    /// @return eToken balance, in internal book-keeping units (18 decimals)
-    function convertUnderlyingToBalance(uint underlyingAmount) external view returns (uint);
-
-    /// @notice Updates interest accumulator and totalBorrows, credits reserves, re-targets interest rate, and logs asset status
-    function touch() external;
 
     /// @notice Transfer underlying tokens from sender to the Euler pool, and increase account's eTokens
     /// @param subAccountId 0 for primary, 1-255 for a sub-account
@@ -56,51 +20,15 @@ interface IEulerEToken {
     /// @param amount In underlying units (use max uint256 for full pool balance)
     function withdraw(uint subAccountId, uint amount) external;
 
-    /// @notice Mint eTokens and a corresponding amount of dTokens ("self-borrow")
-    /// @param subAccountId 0 for primary, 1-255 for a sub-account
-    /// @param amount In underlying units
-    function mint(uint subAccountId, uint amount) external;
-
-    /// @notice Pay off dToken liability with eTokens ("self-repay")
-    /// @param subAccountId 0 for primary, 1-255 for a sub-account
-    /// @param amount In underlying units (use max uint256 to repay the debt in full or up to the available underlying balance)
-    function burn(uint subAccountId, uint amount) external;
-
     /// @notice Allow spender to access an amount of your eTokens in sub-account 0
     /// @param spender Trusted address
     /// @param amount Use max uint256 for "infinite" allowance
     function approve(address spender, uint amount) external returns (bool);
-
-    /// @notice Allow spender to access an amount of your eTokens in a particular sub-account
-    /// @param subAccountId 0 for primary, 1-255 for a sub-account
-    /// @param spender Trusted address
-    /// @param amount Use max uint256 for "infinite" allowance
-    function approveSubAccount(uint subAccountId, address spender, uint amount) external returns (bool);
-
-    /// @notice Retrieve the current allowance
-    /// @param holder Xor with the desired sub-account ID (if applicable)
-    /// @param spender Trusted address
-    function allowance(address holder, address spender) external view returns (uint);
 
     /// @notice Transfer eTokens to another address (from sub-account 0)
     /// @param to Xor with the desired sub-account ID (if applicable)
     /// @param amount In internal book-keeping units (as returned from balanceOf).
     function transfer(address to, uint amount) external returns (bool);
 
-    /// @notice Transfer the full eToken balance of an address to another
-    /// @param from This address must've approved the to address, or be a sub-account of msg.sender
-    /// @param to Xor with the desired sub-account ID (if applicable)
-    function transferFromMax(address from, address to) external returns (bool);
-
-    /// @notice Transfer eTokens from one address to another
-    /// @param from This address must've approved the to address, or be a sub-account of msg.sender
-    /// @param to Xor with the desired sub-account ID (if applicable)
-    /// @param amount In internal book-keeping units (as returned from balanceOf).
-    function transferFrom(address from, address to, uint amount) external returns (bool);
-
-    /// @notice Donate eTokens to the reserves
-    /// @param subAccountId 0 for primary, 1-255 for a sub-account
-    /// @param amount In internal book-keeping units (as returned from balanceOf).
-    function donateToReserves(uint subAccountId, uint amount) external;
 }
 
--- packages/protocol/src/interfaces/euler/IEulerMarkets.sol
+++ packages/protocol/src/interfaces/euler/IEulerMarkets.sol
@@ -10,11 +10,6 @@ interface IEulerMarkets {
     /// @return The created EToken, or the existing EToken if already activated.
     function activateMarket(address underlying) external returns (address);
 
-    /// @notice Create a pToken and activate it on Euler. pTokens are protected wrappers around assets that prevent borrowing.
-    /// @param underlying The address of an ERC20-compliant token. There must already be an activated market on Euler for this underlying, and it must have a non-zero collateral factor.
-    /// @return The created pToken, or an existing one if already activated.
-    function activatePToken(address underlying) external returns (address);
-
     /// @notice Given an underlying, lookup the associated EToken
     /// @param underlying Token address
     /// @return EToken address, or address(0) if not activated
@@ -24,21 +19,6 @@ interface IEulerMarkets {
     /// @param underlying Token address
     /// @return DToken address, or address(0) if not activated
     function underlyingToDToken(address underlying) external view returns (address);
-
-    /// @notice Given an underlying, lookup the associated PToken
-    /// @param underlying Token address
-    /// @return PToken address, or address(0) if it doesn't exist
-    function underlyingToPToken(address underlying) external view returns (address);
-
-    /// @notice Looks up the Euler-related configuration for a token, and resolves all default-value placeholders to their currently configured values.
-    /// @param underlying Token address
-    /// @return Configuration struct
-    function underlyingToAssetConfig(address underlying) external view returns (IEuler.AssetConfig memory);
-
-    /// @notice Looks up the Euler-related configuration for a token, and returns it unresolved (with default-value placeholders)
-    /// @param underlying Token address
-    /// @return config Configuration struct
-    function underlyingToAssetConfigUnresolved(address underlying) external view returns (IEuler.AssetConfig memory config);
 
     /// @notice Given an EToken address, looks up the associated underlying
     /// @param eToken EToken address
@@ -55,50 +35,14 @@ interface IEulerMarkets {
     /// @return dTokenAddr DToken address
     function eTokenToDToken(address eToken) external view returns (address dTokenAddr);
 
-    /// @notice Looks up an asset's currently configured interest rate model
-    /// @param underlying Token address
-    /// @return Module ID that represents the interest rate model (IRM)
-    function interestRateModel(address underlying) external view returns (uint);
-
     /// @notice Retrieves the current interest rate for an asset
     /// @param underlying Token address
     /// @return The interest rate in yield-per-second, scaled by 10**27
     function interestRate(address underlying) external view returns (int96);
-
-    /// @notice Retrieves the current interest rate accumulator for an asset
-    /// @param underlying Token address
-    /// @return An opaque accumulator that increases as interest is accrued
-    function interestAccumulator(address underlying) external view returns (uint);
-
-    /// @notice Retrieves the reserve fee in effect for an asset
-    /// @param underlying Token address
-    /// @return Amount of interest that is redirected to the reserves, as a fraction scaled by RESERVE_FEE_SCALE (4e9)
-    function reserveFee(address underlying) external view returns (uint32);
-
-    /// @notice Retrieves the pricing config for an asset
-    /// @param underlying Token address
-    /// @return pricingType (1=pegged, 2=uniswap3, 3=forwarded, 4=chainlink)
-    /// @return pricingParameters If uniswap3 pricingType then this represents the uniswap pool fee used, if chainlink pricing type this represents the fallback uniswap pool fee or 0 if none
-    /// @return pricingForwarded If forwarded pricingType then this is the address prices are forwarded to, otherwise address(0)
-    function getPricingConfig(address underlying) external view returns (uint16 pricingType, uint32 pricingParameters, address pricingForwarded);
-
-    /// @notice Retrieves the Chainlink price feed config for an asset
-    /// @param underlying Token address
-    /// @return chainlinkAggregator Chainlink aggregator proxy address
-    function getChainlinkPriceFeedConfig(address underlying) external view returns (address chainlinkAggregator);
-
-    /// @notice Retrieves the list of entered markets for an account (assets enabled for collateral or borrowing)
-    /// @param account User account
-    /// @return List of underlying token addresses
-    function getEnteredMarkets(address account) external view returns (address[] memory);
 
     /// @notice Add an asset to the entered market list, or do nothing if already entered
     /// @param subAccountId 0 for primary, 1-255 for a sub-account
     /// @param newMarket Underlying token address
     function enterMarket(uint subAccountId, address newMarket) external;
 
-    /// @notice Remove an asset from the entered market list, or do nothing if not already present
-    /// @param subAccountId 0 for primary, 1-255 for a sub-account
-    /// @param oldMarket Underlying token address
-    function exitMarket(uint subAccountId, address oldMarket) external;
 }
