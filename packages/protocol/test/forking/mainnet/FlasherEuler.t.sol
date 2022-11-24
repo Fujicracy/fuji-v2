7bff122df26cba2882d84a5283f00eecb7a95d4c
pedrovalido <pedro.validex@gmail.com>
2022-11-24 00:35:00 +0000

    refactor(protocol): test simple flashloan with flashereuler

--- packages/protocol/test/forking/mainnet/FlasherEuler.t.sol
+++ packages/protocol/test/forking/mainnet/FlasherEuler.t.sol
@@ -53,113 +53,81 @@ contract FlasherEulerTest is Routines, ForkingSetup, IFlashloan {
     flasher = new FlasherEuler(0x27182842E098f60e3D576794A5bFFb0777E025d3);
 
     // console.log(IERC20(collateralAsset).balanceOf(ALICE));
-    do_depositAndBorrow(DEPOSIT_AMOUNT, 16, vault, ALICE);
+    do_depositAndBorrow(DEPOSIT_AMOUNT, BORROW_AMOUNT, vault, ALICE);
 
-    //wait for block to be mined
-    // vm.roll(block.number + 1);
-    // vm.warp(block.timestamp + 1 minutes);
-  }
-  //
-  // function test_eulerFlashloan() public {
-  //   uint256 asset = DEPOSIT_AMOUNT;
-  //   uint256 debt = 16;
-  //
-  //   IEulerDToken dToken = IEulerDToken(
-  //     IEulerMarkets(0x3520d5a913427E6F0D6A83E07ccD4A4da316e4d3).underlyingToDToken(asset)
-  //   );
-  //   dToken.flashLoan(amount, abi.encode(asset, amount));
-  //
-  //   assertEq(providerAave.getDepositBalance(address(vault), IVault(address(vault))), 0);
-  //   assertEq(providerAave.getBorrowBalance(address(vault), IVault(address(vault))), 0);
-  //
-  //   assertEq(providerEuler.getDepositBalance(address(vault), IVault(address(vault))), assets);
-  //   assertEq(providerEuler.getBorrowBalance(address(vault), IVault(address(vault))), debt);
-  //
-  // }
-
-  // function test_rebalanceWithFlasher() public {
-  //   uint256 assets = DEPOSIT_AMOUNT;
-  //   uint256 debt = 16;
-  //
-  //   bytes memory requestorCall = abi.encodeWithSelector(
-  //     MockRebalancerManager.completeRebalance.selector,
-  //     vault,
-  //     asset,
-  //     debt,
-  //     providerAave,
-  //     providerEuler,
-  //     flasher,
-  //     true
-  //   );
-  //
-  //   //maybe requestor is euler main address
-  //   flasher.initiateFlashloan(assets, debt, address(this), requestorCall);
-  //   assertEq(providerAave.getDepositBalance(address(vault), IVault(address(vault))), 0);
-  //   assertEq(providerAave.getBorrowBalance(address(vault), IVault(address(vault))), 0);
-  //
-  //   assertEq(providerEuler.getDepositBalance(address(vault), IVault(address(vault))), assets);
-  //   assertEq(providerEuler.getBorrowBalance(address(vault), IVault(address(vault))), debt);
-  // }
-
-  function test_simpleFlashloan() public {
-    IEulerDToken dToken = IEulerDToken(
-      IEulerMarkets(0x3520d5a913427E6F0D6A83E07ccD4A4da316e4d3).underlyingToDToken(debtAsset)
-    );
-    bytes memory data = abi.encode(0);
-    dToken.flashLoan(BORROW_AMOUNT, data);
+    // wait for block to be mined
+    vm.roll(block.number + 1);
+    vm.warp(block.timestamp + 1 minutes);
   }
 
   function onFlashLoan(bytes memory data) external {
-    uint256 assets = DEPOSIT_AMOUNT;
-    uint256 debt = BORROW_AMOUNT;
+    (address debtAsset_, uint256 amount_) = abi.decode(data, (address, uint256));
 
-    console.log("initial");
-    console.log("balance collateral - ", IERC20(collateralAsset).balanceOf(address(this)));
-    console.log("balance debt - ", IERC20(debtAsset).balanceOf(address(this)));
+    assertEq(IERC20(debtAsset_).balanceOf(address(this)), amount_);
 
-    console.log("@onFlashloan inside test");
-
-    // IERC20(debtAsset).approve(address(providerAave), BORROW_AMOUNT);
-    IERC20(debtAsset).transfer(address(providerAave), BORROW_AMOUNT);
-    //amount, IVault
-    providerAave.payback(BORROW_AMOUNT, vault);
-    console.log("after payback provider origin");
-    console.log("balance collateral - ", IERC20(collateralAsset).balanceOf(address(this)));
-    console.log("balance debt - ", IERC20(debtAsset).balanceOf(address(this)));
-
-    providerAave.withdraw(DEPOSIT_AMOUNT, vault);
-    console.log("after withdraw provider origin");
-    console.log("balance collateral - ", IERC20(collateralAsset).balanceOf(address(this)));
-    console.log("balance debt - ", IERC20(debtAsset).balanceOf(address(this)));
-
-    providerEuler.deposit(DEPOSIT_AMOUNT, vault);
-    console.log("after deposit provider destination");
-    console.log("balance collateral - ", IERC20(collateralAsset).balanceOf(address(this)));
-    console.log("balance debt - ", IERC20(debtAsset).balanceOf(address(this)));
-
-    providerEuler.borrow(BORROW_AMOUNT, vault);
-    console.log("after borrow provider destination");
-    console.log("balance collateral - ", IERC20(collateralAsset).balanceOf(address(this)));
-    console.log("balance debt - ", IERC20(debtAsset).balanceOf(address(this)));
-
-    IERC20(debtAsset).transfer(msg.sender, BORROW_AMOUNT); // repay
-    console.log("after repay");
-    console.log("balance collateral - ", IERC20(collateralAsset).balanceOf(address(this)));
-    console.log("balance debt - ", IERC20(debtAsset).balanceOf(address(this)));
-
-    assertEq(providerAave.getDepositBalance(address(vault), IVault(address(vault))), 0);
-    assertEq(providerAave.getBorrowBalance(address(vault), IVault(address(vault))), 0);
-
-    assertEq(
-      providerEuler.getDepositBalance(address(vault), IVault(address(vault))), DEPOSIT_AMOUNT
-    );
-    assertEq(providerEuler.getBorrowBalance(address(vault), IVault(address(vault))), BORROW_AMOUNT);
+    IERC20(debtAsset_).transfer(msg.sender, amount_); // repay
   }
 
-  //test rebalance a full position to another provider
+  function test_flashloan() public {
+    IEulerDToken dToken = IEulerDToken(
+      IEulerMarkets(0x3520d5a913427E6F0D6A83E07ccD4A4da316e4d3).underlyingToDToken(debtAsset)
+    );
+    bytes memory data = abi.encode(debtAsset, BORROW_AMOUNT);
+    dToken.flashLoan(BORROW_AMOUNT, data);
+    assertEq(IERC20(debtAsset).balanceOf(address(this)), 0);
+  }
+
+  // function onFlashLoan1(bytes memory data) external {
+  //   // uint256 assets = DEPOSIT_AMOUNT;
+  //   // uint256 debt = BORROW_AMOUNT;
+  //
+  //   console.log("initial");
+  //   console.log("balance collateral - ", IERC20(collateralAsset).balanceOf(address(this)));
+  //   console.log("balance debt - ", IERC20(debtAsset).balanceOf(address(this)));
+  //
+  //   console.log("@onFlashloan inside test");
+  //
+  //   // IERC20(debtAsset).approve(address(providerAave), BORROW_AMOUNT);
+  //   IERC20(debtAsset).transfer(address(providerAave), BORROW_AMOUNT);
+  //   //amount, IVault
+  //   providerAave.payback(BORROW_AMOUNT, vault);
+  //   console.log("after payback provider origin");
+  //   console.log("balance collateral - ", IERC20(collateralAsset).balanceOf(address(this)));
+  //   console.log("balance debt - ", IERC20(debtAsset).balanceOf(address(this)));
+  //
+  //   providerAave.withdraw(DEPOSIT_AMOUNT, vault);
+  //   console.log("after withdraw provider origin");
+  //   console.log("balance collateral - ", IERC20(collateralAsset).balanceOf(address(this)));
+  //   console.log("balance debt - ", IERC20(debtAsset).balanceOf(address(this)));
+  //
+  //   providerEuler.deposit(DEPOSIT_AMOUNT, vault);
+  //   console.log("after deposit provider destination");
+  //   console.log("balance collateral - ", IERC20(collateralAsset).balanceOf(address(this)));
+  //   console.log("balance debt - ", IERC20(debtAsset).balanceOf(address(this)));
+  //
+  //   providerEuler.borrow(BORROW_AMOUNT, vault);
+  //   console.log("after borrow provider destination");
+  //   console.log("balance collateral - ", IERC20(collateralAsset).balanceOf(address(this)));
+  //   console.log("balance debt - ", IERC20(debtAsset).balanceOf(address(this)));
+  //
+  //   IERC20(debtAsset).transfer(msg.sender, BORROW_AMOUNT); // repay
+  //   console.log("after repay");
+  //   console.log("balance collateral - ", IERC20(collateralAsset).balanceOf(address(this)));
+  //   console.log("balance debt - ", IERC20(debtAsset).balanceOf(address(this)));
+  //
+  //   assertEq(providerAave.getDepositBalance(address(vault), IVault(address(vault))), 0);
+  //   assertEq(providerAave.getBorrowBalance(address(vault), IVault(address(vault))), 0);
+  //
+  //   assertEq(
+  //     providerEuler.getDepositBalance(address(vault), IVault(address(vault))), DEPOSIT_AMOUNT
+  //   );
+  //   assertEq(providerEuler.getBorrowBalance(address(vault), IVault(address(vault))), BORROW_AMOUNT);
+  // }
+
+  // test rebalance a full position to another provider
   // function test_rebalanceWithRebalancer() public {
   //   uint256 assets = DEPOSIT_AMOUNT;
-  //   uint256 debt = 16;
+  //   uint256 debt = BORROW_AMOUNT;
   //
   //   rebalancer.rebalanceVault(vault, assets, debt, providerAave, providerEuler, flasher, true);
   //
