9d5a704c568d8aa003d4b25025155e04702dd21d
Boyan Barakov <boyan.barakov@gmail.com>
2022-11-28 14:09:05 +0100

    refactor(protocol): add new SSTORE2 lib and deploy with assembly

--- packages/protocol/src/vaults/borrowing/BorrowingVaultFactory.sol
+++ packages/protocol/src/vaults/borrowing/BorrowingVaultFactory.sol
@@ -1,31 +1,68 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.15;
 
-import {BorrowingVault} from "./BorrowingVault.sol";
 import {VaultDeployer} from "../../abstracts/VaultDeployer.sol";
+import {LibSSTORE2} from "../../libraries/LibSSTORE2.sol";
 import {IERC20Metadata} from
   "openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract BorrowingVaultFactory is VaultDeployer {
+  error BorrowingVaultFactory__deployVault_failed();
+
+  event DeployBorrowingVault(
+    address indexed vault,
+    address indexed asset,
+    address indexed debtAsset,
+    string name,
+    string symbol,
+    bytes32 salt
+  );
+
   uint256 public nonce;
+
+  address public creationAddress;
 
   constructor(address _chief) VaultDeployer(_chief) {}
 
-  function deployVault(bytes memory _deployData) external onlyChief returns (address vault) {
+  /**
+   * Deploys a new "BorrowingVault".
+   * @param deployData The encoded data containing asset, debtAsset and oracle.
+   */
+  function deployVault(bytes memory deployData) external onlyChief returns (address vault) {
     (address asset, address debtAsset, address oracle) =
-      abi.decode(_deployData, (address, address, address));
+      abi.decode(deployData, (address, address, address));
 
-    string memory assetName = IERC20Metadata(asset).name();
     string memory assetSymbol = IERC20Metadata(asset).symbol();
+    string memory debtSymbol = IERC20Metadata(debtAsset).symbol();
 
-    // name_, ex: Fuji-V2 Dai Stablecoin BorrowingVault Shares
-    string memory name = string(abi.encodePacked("Fuji-V2 ", assetName, " BorrowingVault Shares"));
-    // symbol_, ex: fbvDAI
-    string memory symbol = string(abi.encodePacked("fbv", assetSymbol));
+    // name_, ex: Fuji-V2 WETH-DAI BorrowingVault
+    string memory name =
+      string(abi.encodePacked("Fuji-V2 ", assetSymbol, "-", debtSymbol, " BorrowingVault"));
+    // symbol_, ex: fbvWETHDAI
+    string memory symbol = string(abi.encodePacked("fbv", assetSymbol, debtSymbol));
 
-    bytes32 salt = keccak256(abi.encode(_deployData, nonce));
+    bytes32 salt = keccak256(abi.encode(deployData, nonce));
     nonce++;
-    vault = address(new BorrowingVault{salt: salt}(asset, debtAsset, oracle, chief, name, symbol));
+
+    bytes memory bytecode = abi.encodePacked(
+      LibSSTORE2.read(creationAddress), abi.encode(asset, debtAsset, oracle, chief, name, symbol)
+    );
+
+    assembly {
+      vault := create2(0, add(bytecode, 32), mload(bytecode), salt)
+    }
+    if (vault == address(0)) revert BorrowingVaultFactory__deployVault_failed();
+
     _registerVault(vault, asset, salt);
+
+    emit DeployBorrowingVault(vault, asset, debtAsset, name, symbol, salt);
+  }
+
+  /**
+   * Sets the bytecode for the BorrowingVault.
+   * @param creationCode The creationCode for the vault contract.
+   */
+  function setContractCode(bytes calldata creationCode) external onlyTimelock {
+    creationAddress = LibSSTORE2.write(creationCode);
   }
 }
