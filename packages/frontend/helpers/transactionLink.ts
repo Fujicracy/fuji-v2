00b5a22e5935eae0a20f9e1b571039b00ff5af10
Guillaume Lodi <guillaume.lodi@gmail.com>
2022-12-02 17:37:22 +0100

    fix(frontend): ret undefined instead of void (typing)

--- packages/frontend/helpers/transactionLink.ts
+++ packages/frontend/helpers/transactionLink.ts
@@ -9,9 +9,10 @@ chains.map((c) => {
 export function transactionLink(id: string | number, hash: string) {
   const link = chainsMap.get(id)
   if (!link) {
-    return console.error(`No blockExplorerUrl found for chainId ${id}.
+    console.error(`No blockExplorerUrl found for chainId ${id}.
     - Make sure id is valid (hex string or base 10 number) and the chain is setup in web3-onboard config
     - Make sure blockExplorerUrl is set for chain ${id} in web3-onboard config`)
+    return
   }
   return link + "tx/" + hash
 }
