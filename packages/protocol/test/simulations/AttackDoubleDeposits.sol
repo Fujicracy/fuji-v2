467411aa3ec6171b8ef168b93ade07120d6d22b2
DCota <32775237+DaigaroCota@users.noreply.github.com>
2022-12-06 13:20:41 -0700

    test(protocol): two deposits aave and compound

--- packages/protocol/test/simulations/AttackDoubleDeposits.sol
+++ packages/protocol/test/simulations/AttackDoubleDeposits.sol
@@ -8,22 +8,22 @@ import {IERC20Metadata} from
 import {TimelockController} from
   "openzeppelin-contracts/contracts/governance/TimelockController.sol";
 import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
-import {IWETH9} from "../../../src/helpers/PeripheryPayments.sol";
-import {IVault} from "../../../src/interfaces/IVault.sol";
-import {ICToken} from "../../../src/interfaces/compoundV2/ICToken.sol";
-import {BorrowingVault} from "../../../src/vaults/borrowing/BorrowingVault.sol";
-import {CompoundV2} from "../../../src/providers/mainnet/CompoundV2.sol";
-import {IComptroller} from "../../../src/interfaces/compoundV2/IComptroller.sol";
-import {AaveV2} from "../../../src/providers/mainnet/AaveV2.sol";
-import {ILendingProvider} from "../../../src/interfaces/ILendingProvider.sol";
-import {MockOracle} from "../../../src/mocks/MockOracle.sol";
-import {Chief} from "../../../src/Chief.sol";
-import {CoreRoles} from "../../../src/access/CoreRoles.sol";
-import {DSTestPlus} from "../../utils/DSTestPlus.sol";
+import {IWETH} from "../../src/interfaces/IWETH.sol";
+import {IVault} from "../../src/interfaces/IVault.sol";
+import {ICToken} from "../../src/interfaces/compoundV2/ICToken.sol";
+import {BorrowingVault} from "../../src/vaults/borrowing/BorrowingVault.sol";
+import {CompoundV2} from "../../src/providers/mainnet/CompoundV2.sol";
+import {IComptroller} from "../../src/interfaces/compoundV2/IComptroller.sol";
+import {AaveV2} from "../../src/providers/mainnet/AaveV2.sol";
+import {ILendingProvider} from "../../src/interfaces/ILendingProvider.sol";
+import {MockOracle} from "../../src/mocks/MockOracle.sol";
+import {Chief} from "../../src/Chief.sol";
+import {CoreRoles} from "../../src/access/CoreRoles.sol";
+import {DSTestPlus} from "../utils/DSTestPlus.sol";
 
 bool constant DEBUG = false;
 
-contract CompoundV2AttackTest is DSTestPlus, CoreRoles {
+contract AttackDoubleDeposit is DSTestPlus, CoreRoles {
   address alice = address(0xA);
   address bob = address(0xB);
 
@@ -31,10 +31,11 @@ contract CompoundV2AttackTest is DSTestPlus, CoreRoles {
 
   IVault public vault;
   CompoundV2 public compoundV2;
+  AaveV2 public aaveV2;
   Chief public chief;
   TimelockController public timelock;
 
-  IWETH9 public weth;
+  IWETH public weth;
   IERC20 public usdc;
 
   MockOracle mockOracle;
@@ -45,7 +46,7 @@ contract CompoundV2AttackTest is DSTestPlus, CoreRoles {
   function setUp() public {
     mainnetFork = vm.createSelectFork("mainnet");
 
-    weth = IWETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    weth = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
     usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
 
     vm.label(address(alice), "alice");
@@ -55,8 +56,8 @@ contract CompoundV2AttackTest is DSTestPlus, CoreRoles {
 
     mockOracle = new MockOracle();
 
-    mockOracle.setPriceOf(address(weth), address(usdc), 62500);
-    mockOracle.setPriceOf(address(usdc), address(weth), 160000000000);
+    _utils_setPrice(address(weth), address(usdc), 62500);
+    _utils_setPrice(address(usdc), address(weth), 160000000000);
 
     address[] memory admins = new address[](1);
     admins[0] = address(this);
@@ -75,11 +76,21 @@ contract CompoundV2AttackTest is DSTestPlus, CoreRoles {
     );
 
     compoundV2 = new CompoundV2();
-    ILendingProvider[] memory providers = new ILendingProvider[](1);
+    aaveV2 = new AaveV2();
+    ILendingProvider[] memory providers = new ILendingProvider[](2);
     providers[0] = compoundV2;
+    providers[1] = aaveV2;
 
     _utils_setupVaultProvider(vault, providers);
     vault.setActiveProvider(compoundV2);
+  }
+
+  function _utils_setPrice(address asset1, address asset2, uint256 price) internal {
+    vm.mockCall(
+      address(mockOracle),
+      abi.encodeWithSelector(MockOracle.getPriceOf.selector, asset1, asset2, 18),
+      abi.encode(price)
+    );
   }
 
   function _utils_setupTestRoles() internal {
@@ -138,15 +149,25 @@ contract CompoundV2AttackTest is DSTestPlus, CoreRoles {
     assertEq(v.convertToAssets(v.balanceOf(who)), diff);
   }
 
-  function test_twoDeposits() public {
+  function test_twoDepositsInCompoundV2() public {
     // Two deposits are reverting because of overflow in maxMin function
     deal(address(weth), alice, DEPOSIT_AMOUNT);
     deal(address(weth), bob, DEPOSIT_AMOUNT);
 
-    address cTokenAddr = compoundV2.getMapper().getAddressMapping(
-      compoundV2.providerName(), 
-      address(weth));
-    ICToken cToken = ICToken(cTokenAddr);
+    // address cTokenAddr = compoundV2.getMapper().getAddressMapping(
+    //   compoundV2.providerName(),
+    //   address(weth));
+    // ICToken cToken = ICToken(cTokenAddr);
+
+    _utils_doDeposit(bob, DEPOSIT_AMOUNT, vault);
+    _utils_doDeposit(alice, DEPOSIT_AMOUNT, vault);
+  }
+
+  function test_twoDepositsInAaveV2() public {
+    vault.setActiveProvider(aaveV2);
+    // Two deposits are reverting because of overflow in maxMin function
+    deal(address(weth), alice, DEPOSIT_AMOUNT);
+    deal(address(weth), bob, DEPOSIT_AMOUNT);
 
     _utils_doDeposit(bob, DEPOSIT_AMOUNT, vault);
     _utils_doDeposit(alice, DEPOSIT_AMOUNT, vault);
