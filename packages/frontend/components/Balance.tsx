e96504429105d6d8711db72e94689b2bb003cdbd
Guillaume Lodi <guillaume.lodi@gmail.com>
2022-12-07 15:50:26 +0100

    fix(balance): wrong display on small amounts

--- packages/frontend/components/Balance.tsx
+++ packages/frontend/components/Balance.tsx
@@ -2,21 +2,29 @@ import { Token } from "@x-fuji/sdk"
 
 type Props = {
   balance?: number
-  token?: Token
+  /**
+   * Allow rounding.
+   * @default false
+   * @example 1,200.42 -> 1.2K,
+   * @example 3,234,123 -> 3,2M
+   */
+  rounding?: boolean
+  symbol?: string
   dataCy?: string
 }
 
 // Format the balance depending of the token
 // If no token is specified, 5 digits is used
 // else, eth based tokens use 4 digits
-const Balance = ({ balance, token, dataCy }: Props) => {
-  const bal = balance?.toLocaleString() ?? "0"
-  const balLength = token?.symbol.includes("ETH") ? 5 : 5
-  const formattedBalance = bal.substring(0, balLength)
+const Balance = ({ balance, rounding, symbol, dataCy }: Props) => {
+  const formattedBalance =
+    balance?.toLocaleString("en-US", {
+      notation: rounding ? "compact" : "standard",
+    }) ?? "0"
 
   return (
     <span id="balance-amount" data-cy={dataCy}>
-      {formattedBalance}
+      {formattedBalance} {symbol}
     </span>
   )
 }
