d69f6bf394dbea6a56d6bbbd526b7078c4daaac3
Guillaume Lodi <guillaume.lodi@gmail.com>
2022-12-09 11:15:39 +0100

    fix(frontend): delete svg icon style bc it prevents us from using color props

--- packages/frontend/styles/theme.ts
+++ packages/frontend/styles/theme.ts
@@ -404,13 +404,6 @@ const theme = createTheme(colorTheme, {
         },
       },
     },
-    MuiSvgIcon: {
-      styleOverrides: {
-        root: {
-          color: colorTheme.palette.text.secondary,
-        },
-      },
-    },
     MuiLink: {
       styleOverrides: {
         root: {
