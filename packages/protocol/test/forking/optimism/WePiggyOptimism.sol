de6811a7a05c2de5182504205cdcd620b06cfaae
pedrovalido <pedro.validex@gmail.com>
2022-12-13 01:19:49 +0000

    test(protocol): wepiggy test for optimism

---
+++ packages/protocol/test/forking/optimism/WePiggyOptimism.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.15;
+
+import "forge-std/console.sol";
+import {Routines} from "../../utils/Routines.sol";
+import {ForkingSetup} from "../ForkingSetup.sol";
+import {ILendingProvider} from "../../../src/interfaces/ILendingProvider.sol";
+import {BorrowingVault} from "../../../src/vaults/borrowing/BorrowingVault.sol";
+import {WePiggyOptimism} from "../../../src/providers/optimism/WePiggyOptimism.sol";
+
+contract WePiggyOptimismTest is Routines, ForkingSetup {
+  ILendingProvider public wePiggy;
+
+  uint256 public constant DEPOSIT_AMOUNT = 0.5 ether;
+  uint256 public constant BORROW_AMOUNT = 200;
+
+  function setUp() public {
+    // deploy(MAINNET_DOMAIN);
+    deploy(OPTIMISM_DOMAIN);
+
+    wePiggy = new WePiggyOptimism();
+    ILendingProvider[] memory providers = new ILendingProvider[](1);
+    providers[0] = wePiggy;
+
+    _setVaultProviders(vault, providers);
+    vault.setActiveProvider(wePiggy);
+  }
+
+  function test_depositAndBorrow() public {
+    do_deposit(DEPOSIT_AMOUNT, vault, ALICE);
+    do_borrow(BORROW_AMOUNT, vault, ALICE);
+  }
+
+  function test_paybackAndWithdraw() public {
+    deal(address(vault.asset()), ALICE, DEPOSIT_AMOUNT);
+
+    do_depositAndBorrow(DEPOSIT_AMOUNT, BORROW_AMOUNT, vault, ALICE);
+
+    vm.roll(block.number + 1);
+    vm.warp(block.timestamp + 1 minutes);
+
+    uint256 aliceDebt = vault.balanceOfDebt(ALICE);
+    do_payback(aliceDebt, vault, ALICE);
+
+    uint256 maxAmount = vault.maxWithdraw(ALICE);
+    do_withdraw(maxAmount, vault, ALICE);
+  }
+
+  function test_getBalances() public {
+    do_depositAndBorrow(DEPOSIT_AMOUNT, BORROW_AMOUNT, vault, ALICE);
+
+    uint256 depositBalance = vault.totalAssets();
+    uint256 borrowBalance = vault.totalDebt();
+    assertGe(depositBalance, DEPOSIT_AMOUNT);
+    assertGe(borrowBalance, BORROW_AMOUNT);
+  }
+
+  function test_getInterestRates() public {
+    uint256 depositRate = wePiggy.getDepositRateFor(vault);
+    assertGt(depositRate, 0); // Should be greater than zero.
+
+    uint256 borrowRate = wePiggy.getBorrowRateFor(vault);
+    assertGt(borrowRate, 0); // Should be greater than zero.
+  }
+}
