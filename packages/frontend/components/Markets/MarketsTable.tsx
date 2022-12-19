72b36505df65548cd1d291d16f83a4761b6abf54
Guillaume Lodi <guillaume.lodi@gmail.com>
2022-12-19 11:54:54 +0100

    feat(frontend): add basic sorting, fix my comments

--- packages/frontend/components/Markets/MarketsTable.tsx
+++ packages/frontend/components/Markets/MarketsTable.tsx
@@ -1,5 +1,6 @@
 import {
   CircularProgress,
+  Link,
   Stack,
   Table,
   TableBody,
@@ -31,168 +32,34 @@ export type Row = {
   isChild: boolean
 }
 
-/* const rows: Row[] = [
-  {
-    borrow: "DAI",
-    collateral: "WETH",
-    bestRateChain: "Ethereum",
-    supplyAPI: 1.8,
-    borrowAPR: 2.25,
-    integratedProtocols: ["AAVE", "COMP"],
-    safetyRating: "A+",
-    availableLiquidity: 164800,
-    isChild: false,
-  },
-  {
-    borrow: "USDC",
-    collateral: "ETH",
-    bestRateChain: "Ethereum",
-    supplyAPI: 1.8,
-    borrowAPR: 2.55,
-    integratedProtocols: ["AAVE", "COMP"],
-    safetyRating: "A+",
-    availableLiquidity: 164800,
-    isChild: false,
-    children: [
-      {
-        borrow: null,
-        collateral: null,
-        bestRateChain: "Ethereum",
-        supplyAPI: 1.8,
-        borrowAPR: 2.55,
-        integratedProtocols: ["AAVE", "COMP"],
-        safetyRating: "A+",
-        availableLiquidity: 24800,
-        isChild: true,
-      },
-      {
-        borrow: null,
-        collateral: null,
-        bestRateChain: "Polygon",
-        supplyAPI: 1.5,
-        borrowAPR: 2.75,
-        integratedProtocols: ["AAVE", "COMP"],
-        safetyRating: "A+",
-        availableLiquidity: 124800,
-        isChild: true,
-      },
-      {
-        borrow: null,
-        collateral: null,
-        bestRateChain: "Fantom",
-        supplyAPI: 1.5,
-        borrowAPR: 2.95,
-        integratedProtocols: ["AAVE", "COMP"],
-        safetyRating: "A+",
-        availableLiquidity: 24800,
-        isChild: true,
-      },
-      {
-        borrow: null,
-        collateral: null,
-        bestRateChain: "Optimism",
-        supplyAPI: 1.3,
-        borrowAPR: 2.98,
-        integratedProtocols: ["AAVE", "COMP"],
-        safetyRating: "A+",
-        availableLiquidity: 88000,
-        isChild: true,
-      },
-      {
-        borrow: null,
-        collateral: null,
-        bestRateChain: "Arbitrum",
-        supplyAPI: 1.3,
-        borrowAPR: 2.99,
-        integratedProtocols: ["AAVE", "COMP"],
-        safetyRating: "A+",
-        availableLiquidity: 100000,
-        isChild: true,
-        children: [
-          {
-            borrow: null,
-            collateral: null,
-            bestRateChain: "Arbitrum",
-            supplyAPI: 1.3,
-            borrowAPR: 3.01,
-            integratedProtocols: ["AAVE", "COMP"],
-            safetyRating: "A+",
-            availableLiquidity: 100000,
-            isChild: true,
-          },
-          {
-            borrow: null,
-            collateral: null,
-            bestRateChain: "Arbitrum",
-            supplyAPI: 1.3,
-            borrowAPR: 3.02,
-            integratedProtocols: ["WPC", "IB", "SONNE", "COMP", "AAVE"],
-            safetyRating: "B+",
-            availableLiquidity: 100000,
-            isChild: true,
-          },
-        ],
-      },
-    ],
-  },
-  {
-    borrow: "USDC",
-    collateral: "ETH",
-    bestRateChain: "Ethereum",
-    supplyAPI: 1.8,
-    borrowAPR: 2.55,
-    integratedProtocols: ["AAVE", "COMP"],
-    safetyRating: "A+",
-    availableLiquidity: 24800,
-    isChild: false,
-  },
-  {
-    borrow: "USDT",
-    collateral: "ETH",
-    bestRateChain: "Ethereum",
-    supplyAPI: 1.8,
-    borrowAPR: 2.65,
-    integratedProtocols: ["WPC", "IB", "SONNE", "COMP", "AAVE"],
-    safetyRating: "B+",
-    availableLiquidity: 164800,
-    isChild: false,
-  },
-] */
+type SortBy = "descending" | "ascending"
 
 export default function MarketsTable() {
   const { palette } = useTheme()
-  const [aprSorting, setAprSorting] = useState("descending")
+  const [appSorting, setAppSorting] = useState<SortBy>("descending")
   const [vaults, setVaults] = useState<void | BorrowingVaultWithFinancials[]>(
     []
   )
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    sdk.getAllVaultsWithFinancials().then((v) => {
-      setVaults(v)
-      setLoading(false)
-    })
+    async function fetchVaults() {
+      try {
+        const v = await sdk.getAllVaultsWithFinancials()
+        setVaults(v)
+        setLoading(false)
+      } catch (e) {
+        // TODO: What if error
+        setLoading(false)
+      }
+    }
+    fetchVaults()
   }, [])
 
-  console.log(vaults)
-
-  const rows: Row[] = []
-
-  vaults?.forEach((vault) => {
-    //vault.vault.getProviders().then((providers) => {
-    //console.log(providers.reduce(provider => provider.name));
-    rows.push({
-      borrow: vault.vault.debt.symbol,
-      collateral: vault.vault.collateral.symbol,
-      bestRateChain: "Ethereum",
-      supplyAPI: vault.depositApyBase,
-      borrowAPR: vault.borrowApyBase,
-      integratedProtocols: ["AAVE", "COMP"],
-      safetyRating: "A+",
-      availableLiquidity: vault.availableToBorrowUSD,
-      isChild: false,
-    })
-  })
+  const rows: Row[] = vaults
+    ? vaults.map(vaultToRow).sort(sortBy[appSorting])
+    : []
+  console.log({ vaults, rows })
 
   if (loading) {
     return (
@@ -200,130 +67,150 @@ export default function MarketsTable() {
         <CircularProgress sx={{ color: palette.primary.main }} />
       </Stack>
     )
-  } else {
-    return (
-      <TableContainer>
-        <Table
-          aria-label="Markets table"
-          sx={{
-            // border-collapse fix bug on borders on firefox with sticky column
-            borderCollapse: "initial",
-            ".MuiTableCell-root": {
-              minWidth: "160px",
-            },
-          }}
-        >
-          <TableHead>
-            <TableRow sx={{ height: "2.625rem" }}>
-              <TableCell
-                sx={{
-                  position: "sticky",
-                  left: 0,
-                  zIndex: 1,
-                  background: palette.secondary.contrastText,
-                }}
-                align="center"
-              >
-                Borrow
-              </TableCell>
-              <TableCell align="center">Collateral</TableCell>
-              <TableCell align="center">Chain with the best rate</TableCell>
-              <TableCell align="right">Supply APY</TableCell>
-              <TableCell align="right">
-                <Stack
-                  direction="row"
-                  spacing="0.25rem"
-                  alignItems="center"
-                  justifyContent="right"
-                  sx={{ cursor: "pointer" }}
-                  onClick={() =>
-                    setAprSorting(
-                      aprSorting === "ascending" ? "descending" : "ascending"
-                    )
-                  }
-                >
-                  <span>Borrow APR</span>
-                  {aprSorting === "descending" ? (
-                    <KeyboardArrowUpIcon
-                      sx={{
-                        color: palette.info.main,
-                        fontSize: "0.875rem",
-                      }}
-                    />
-                  ) : (
-                    <KeyboardArrowDownIcon
-                      sx={{
-                        color: palette.info.main,
-                        fontSize: "0.875rem",
-                      }}
-                    />
-                  )}
-                </Stack>
-              </TableCell>
-              <TableCell align="right">
-                <Stack
-                  direction="row"
-                  alignItems="center"
-                  spacing="0.25rem"
-                  justifyContent="right"
-                >
-                  <Tooltip
-                    arrow
-                    title="FujiV2 refinances between these protocols to find the best yield"
-                    placement="top"
-                  >
-                    <InfoOutlinedIcon
-                      sx={{
-                        fontSize: "0.875rem",
-                        color: palette.info.main,
-                      }}
-                    />
-                  </Tooltip>
-                  <span>Protocols</span>
-                </Stack>
-              </TableCell>
-              <TableCell>
-                <Stack
-                  direction="row"
-                  alignItems="center"
-                  spacing="0.25rem"
-                  justifyContent="right"
-                >
-                  <Tooltip
-                    arrow
-                    title={
-                      <span>
-                        We take into account variables such as liquidity, audits
-                        and team behind each protocol, you can read more on our
-                        risk framework{" "}
-                        <a
-                          href="https://docs.fujidao.org/"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <u> here</u>
-                        </a>
-                      </span>
-                    }
-                    placement="top"
-                  >
-                    <InfoOutlinedIcon
-                      sx={{ fontSize: "0.875rem", color: palette.info.main }}
-                    />
-                  </Tooltip>
-                  <span>Safety Rating</span>
-                </Stack>
-              </TableCell>
-              <TableCell align="right">Liquidity</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row, i) => (
-              <MarketsTableRow key={i} row={row} extra={i === 0} />
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    )
   }
+  return (
+    <TableContainer>
+      <Table
+        aria-label="Markets table"
+        sx={{
+          // border-collapse fix bug on borders on firefox with sticky column
+          borderCollapse: "initial",
+          ".MuiTableCell-root": {
+            minWidth: "160px",
+          },
+        }}
+      >
+        <TableHead>
+          <TableRow sx={{ height: "2.625rem" }}>
+            <TableCell
+              sx={{
+                position: "sticky",
+                left: 0,
+                zIndex: 1,
+                background: palette.secondary.contrastText,
+              }}
+              align="center"
+            >
+              Borrow
+            </TableCell>
+            <TableCell align="center">Collateral</TableCell>
+            <TableCell align="center">Chain with the best rate</TableCell>
+            <TableCell align="right">Supply APY</TableCell>
+            <TableCell align="right">
+              <Stack
+                direction="row"
+                spacing="0.25rem"
+                alignItems="center"
+                justifyContent="right"
+                sx={{ cursor: "pointer" }}
+                onClick={() =>
+                  setAppSorting(
+                    appSorting === "ascending" ? "descending" : "ascending"
+                  )
+                }
+              >
+                <span>Borrow APR</span>
+                {appSorting === "descending" ? (
+                  <KeyboardArrowUpIcon
+                    sx={{
+                      color: palette.info.main,
+                      fontSize: "0.875rem",
+                    }}
+                  />
+                ) : (
+                  <KeyboardArrowDownIcon
+                    sx={{
+                      color: palette.info.main,
+                      fontSize: "0.875rem",
+                    }}
+                  />
+                )}
+              </Stack>
+            </TableCell>
+            <TableCell align="right">
+              <Stack
+                direction="row"
+                alignItems="center"
+                spacing="0.25rem"
+                justifyContent="right"
+              >
+                <Tooltip
+                  arrow
+                  title="FujiV2 refinances between these protocols to find the best yield"
+                  placement="top"
+                >
+                  <InfoOutlinedIcon
+                    sx={{
+                      fontSize: "0.875rem",
+                      color: palette.info.main,
+                    }}
+                  />
+                </Tooltip>
+                <span>Protocols</span>
+              </Stack>
+            </TableCell>
+            <TableCell>
+              <Stack
+                direction="row"
+                alignItems="center"
+                spacing="0.25rem"
+                justifyContent="right"
+              >
+                <Tooltip
+                  arrow
+                  title={
+                    <span>
+                      We take into account variables such as liquidity, audits
+                      and team behind each protocol, you can read more on our
+                      risk framework{" "}
+                      <Link
+                        href="https://docs.fujidao.org/"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <u> here</u>
+                      </Link>
+                    </span>
+                  }
+                  placement="top"
+                >
+                  <InfoOutlinedIcon
+                    sx={{ fontSize: "0.875rem", color: palette.info.main }}
+                  />
+                </Tooltip>
+                <span>Safety Rating</span>
+              </Stack>
+            </TableCell>
+            <TableCell align="right">Liquidity</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row, i) => (
+            <MarketsTableRow key={i} row={row} extra={i === 0} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+function vaultToRow(vault: BorrowingVaultWithFinancials): Row {
+  return {
+    borrow: vault.vault.debt.symbol,
+    collateral: vault.vault.collateral.symbol,
+    bestRateChain: "Ethereum",
+    supplyAPI: vault.depositApyBase,
+    borrowAPR: vault.borrowApyBase,
+    integratedProtocols: ["AAVE", "COMP"],
+    safetyRating: "A+",
+    availableLiquidity: vault.availableToBorrowUSD,
+    isChild: false,
+  }
+}
+
+type CompareFn = (r1: Row, r2: Row) => 1 | -1
+
+const sortBy: Record<SortBy, CompareFn> = {
+  ascending: (a, b) => (a.borrowAPR < b.borrowAPR ? 1 : -1),
+  descending: (a, b) => (a.borrowAPR > b.borrowAPR ? 1 : -1),
 }
