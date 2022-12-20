eba83deeb8fed9bf74d2abecb3810209e6f234f3
Guillaume Lodi <guillaume.lodi@gmail.com>
2022-12-20 16:32:08 +0100

    fix(frontend): margin on lend page

--- packages/frontend/components/Markets/Lending.tsx
+++ packages/frontend/components/Markets/Lending.tsx
@@ -1,4 +1,12 @@
-import { Button, Card, TextField, Typography, useTheme } from "@mui/material"
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+  useTheme,
+} from "@mui/material"
 import Image from "next/image"
 import { useState } from "react"
 
@@ -8,34 +16,46 @@ export default function Lending() {
 
   return (
     <Card variant="lending" sx={{ p: { xs: "1rem", sm: "3.75rem 27.5rem" } }}>
-      <Image
-        src="/assets/images/logo/himalaya.svg"
-        alt="Logo Himalaya"
-        width={64}
-        height={64}
-        style={{
-          background: palette.secondary.dark,
-          borderRadius: "100%",
-        }}
-      />
-      <Typography variant="h4">Himalaya Lend</Typography>
-      <Typography color={palette.info.main} variant="small">
-        Retail and insituition lending interest rate optimization. Be the first
-        to use our beta platform
-      </Typography>
+      <CardContent sx={{ width: "360px" }}>
+        <Image
+          src="/assets/images/logo/himalaya.svg"
+          alt="Logo Himalaya"
+          width={64}
+          height={64}
+          style={{
+            background: palette.secondary.dark,
+            borderRadius: "100%",
+          }}
+        />
+        <Box mt={3}>
+          <Typography variant="h4" mt={3}>
+            Himalaya Lend
+          </Typography>
+        </Box>
+        <Box mt={1}>
+          <Typography color={palette.info.main} variant="small" mt={2}>
+            Retail and insituition lending interest rate optimization. Be the
+            first to use our beta platform
+          </Typography>
+        </Box>
 
-      <TextField
-        id="email"
-        type="email"
-        placeholder="Enter your email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        fullWidth
-      />
+        <Box mt={3}>
+          <TextField
+            id="email"
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            fullWidth
+          />
+        </Box>
 
-      <Button variant="gradient" fullWidth>
-        Join waitlist
-      </Button>
+        <Box mt={2}>
+          <Button variant="gradient" size="large" fullWidth>
+            Join waitlist
+          </Button>
+        </Box>
+      </CardContent>
     </Card>
   )
 }
