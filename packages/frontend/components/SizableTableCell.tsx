fb095ccf7e40ec2ab7fa5804a6a6071e7c152d5b
Guillaume Lodi <guillaume.lodi@gmail.com>
2022-12-20 15:07:12 +0100

    refactor(frontend): tablecell

--- packages/frontend/components/SizableTableCell.tsx
+++ packages/frontend/components/SizableTableCell.tsx
@@ -2,12 +2,13 @@ import { TableCell, TableCellProps } from "@mui/material"
 
 // Hack bc it's difficult to size a tablecell,
 // to avoid repeating sx=minwidth, maxwidth
+// See https://github.com/mui/material-ui/issues/35536
 type SizableTableCellProps = TableCellProps & {
   width?: string
 }
 export const SizableTableCell = (props: SizableTableCellProps) => {
   const { width, children } = props
-  if (props.sx) console.debug(props.sx)
+
   return (
     <TableCell
       {...props}
