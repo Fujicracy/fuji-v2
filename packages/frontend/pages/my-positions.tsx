303d83bbb5a223bc6b86dbcce0f529f3954130b4
Guillaume Lodi <guillaume.lodi@gmail.com>
2023-01-04 11:46:38 +0100

    fix(frontend): mobile padding and margin

--- packages/frontend/pages/my-positions.tsx
+++ packages/frontend/pages/my-positions.tsx
@@ -94,10 +94,10 @@ const MyPositionPage: NextPage = () => {
 
       <Container
         sx={{
-          mt: { xs: "0", sm: "4rem" },
+          mt: { xs: "2rem", sm: "4rem" },
           mb: { xs: "7rem", sm: "0" },
-          pl: { xs: "0.25rem", sm: "1rem" },
-          pr: { xs: "0.25rem", sm: "1rem" },
+          pl: { xs: "1rem", sm: "1rem" },
+          pr: { xs: "1rem", sm: "1rem" },
           minHeight: "75vh",
         }}
       >
