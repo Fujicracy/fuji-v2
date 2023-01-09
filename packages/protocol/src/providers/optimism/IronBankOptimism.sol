5e23058465bfce984266809cfd8e577298c241c8
pedrovalido <pedro.validex@gmail.com>
2023-01-09 21:05:26 +0000

    fix(protocol): unused imports

--- packages/protocol/src/providers/optimism/IronBankOptimism.sol
+++ packages/protocol/src/providers/optimism/IronBankOptimism.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.15;
 
-import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {IVault} from "../../interfaces/IVault.sol";
 import {ILendingProvider} from "../../interfaces/ILendingProvider.sol";
 import {IAddrMapper} from "../../interfaces/IAddrMapper.sol";
