bcf4dd8d19e154951e017e0b520cadc8f1be4a0c
pedrovalido <pedro.validex@gmail.com>
2023-01-10 17:13:26 +0000

    test(protocol): acrobatic way of calculating balances

--- packages/protocol/test/forking/optimism/IronBankOptimism.t.sol
+++ packages/protocol/test/forking/optimism/IronBankOptimism.t.sol
@@ -7,6 +7,8 @@ import {ForkingSetup} from "../ForkingSetup.sol";
 import {ILendingProvider} from "../../../src/interfaces/ILendingProvider.sol";
 import {BorrowingVault} from "../../../src/vaults/borrowing/BorrowingVault.sol";
 import {IronBankOptimism} from "../../../src/providers/optimism/IronBankOptimism.sol";
+import {IAddrMapper} from "../../../src/interfaces/IAddrMapper.sol";
+import {ICToken} from "../../../src/interfaces/compoundV2/ICToken.sol";
 
 contract IronBankTest is Routines, ForkingSetup {
   ILendingProvider public ironBank;
@@ -51,6 +53,35 @@ contract IronBankTest is Routines, ForkingSetup {
     //account for rounding issue
     assertApproxEqAbs(depositBalance, DEPOSIT_AMOUNT, DEPOSIT_AMOUNT / 1000);
     assertApproxEqAbs(borrowBalance, BORROW_AMOUNT, BORROW_AMOUNT / 1000);
+  }
+
+  function test_getBalancesAcrobatic(uint256 nBlocks) public {
+    vm.assume(nBlocks > 0 && nBlocks < 100000000);
+    do_depositAndBorrow(DEPOSIT_AMOUNT, BORROW_AMOUNT, vault, ALICE);
+
+    uint256 borrowBalance = vault.totalDebt();
+
+    uint256 borrowBalance1 = ICToken(
+      IAddrMapper(0x4cB46032e2790D8CA10be6d0001e8c6362a76adA).getAddressMapping(
+        "IronBank", debtAsset
+      )
+    ).borrowBalanceCurrent(address(vault));
+    assertEq(borrowBalance, borrowBalance1);
+
+    //check after few blocks
+    uint256 time = 2 * nBlocks;
+    vm.warp(block.timestamp + time);
+    vm.roll(block.number + nBlocks);
+
+    borrowBalance = vault.totalDebt();
+
+    borrowBalance1 = ICToken(
+      IAddrMapper(0x4cB46032e2790D8CA10be6d0001e8c6362a76adA).getAddressMapping(
+        "IronBank", debtAsset
+      )
+    ).borrowBalanceCurrent(address(vault));
+
+    assertEq(borrowBalance, borrowBalance1);
   }
 
   function test_getInterestRates() public {
