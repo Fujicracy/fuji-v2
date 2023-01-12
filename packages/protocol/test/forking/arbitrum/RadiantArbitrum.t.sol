b894c78c5a9acd5cba1a7226b49249fe66efcd65
pedrovalido <pedro.validex@gmail.com>
2023-01-12 13:14:27 +0000

    feat(protocol): radiant as lending provider and tests

---
+++ packages/protocol/test/forking/arbitrum/RadiantArbitrum.t.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.15;
+
+import "forge-std/console.sol";
+import {Routines} from "../../utils/Routines.sol";
+import {ForkingSetup} from "../ForkingSetup.sol";
+import {RadiantArbitrum} from "../../../src/providers/arbitrum/RadiantArbitrum.sol";
+import {ILendingProvider} from "../../../src/interfaces/ILendingProvider.sol";
+import {BorrowingVault} from "../../../src/vaults/borrowing/BorrowingVault.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+contract RadiantArbitrumForkingTest is Routines, ForkingSetup {
+  ILendingProvider public radiant;
+
+  uint256 public constant DEPOSIT_AMOUNT = 0.5 ether;
+  uint256 public constant BORROW_AMOUNT = 200 * 1e6;
+
+  function setUp() public {
+    deploy(ARBITRUM_DOMAIN);
+
+    radiant = new RadiantArbitrum();
+    ILendingProvider[] memory providers = new ILendingProvider[](1);
+    providers[0] = radiant;
+
+    _setVaultProviders(vault, providers);
+    vault.setActiveProvider(radiant);
+  }
+
+  function test_depositAndBorrow() public {
+    do_deposit(DEPOSIT_AMOUNT, vault, ALICE);
+    do_borrow(BORROW_AMOUNT, vault, ALICE);
+  }
+
+  function test_paybackAndWithdraw() public {
+    deal(address(vault.asset()), ALICE, DEPOSIT_AMOUNT);
+
+    do_depositAndBorrow(DEPOSIT_AMOUNT, BORROW_AMOUNT, vault, ALICE);
+
+    vm.warp(block.timestamp + 13 seconds);
+    vm.roll(block.number + 1);
+
+    uint256 aliceDebt = vault.balanceOfDebt(ALICE);
+    do_payback(aliceDebt, vault, ALICE);
+
+    assertEq(vault.balanceOfDebt(ALICE), 0);
+
+    uint256 maxAmount = vault.maxWithdraw(ALICE);
+    do_withdraw(maxAmount, vault, ALICE);
+
+    assertGe(IERC20(vault.asset()).balanceOf(ALICE), DEPOSIT_AMOUNT);
+  }
+
+  function test_getBalances() public {
+    do_depositAndBorrow(DEPOSIT_AMOUNT, BORROW_AMOUNT, vault, ALICE);
+
+    uint256 depositBalance = vault.totalAssets();
+    uint256 borrowBalance = vault.totalDebt();
+    assertGe(depositBalance, DEPOSIT_AMOUNT);
+    assertGe(borrowBalance, BORROW_AMOUNT);
+  }
+
+  function test_getInterestRates() public {
+    uint256 depositRate = radiant.getDepositRateFor(vault);
+    assertGt(depositRate, 0); // Should be greater than zero.
+
+    uint256 borrowRate = radiant.getBorrowRateFor(vault);
+    assertGt(borrowRate, 0); // Should be greater than zero.
+  }
+
+  function test_twoDeposits() public {
+    do_deposit(DEPOSIT_AMOUNT, vault, ALICE);
+    do_deposit(DEPOSIT_AMOUNT, vault, BOB);
+  }
+}
