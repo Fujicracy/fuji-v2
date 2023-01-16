37ca812f5abd4c94692efe27b64e02acfbf58c9b
pedrovalido <pedro.validex@gmail.com>
2023-01-16 16:04:21 +0000

    fix(protocol): approved operator, forking refactor

--- packages/protocol/test/forking/gnosis/AgaveGnosis.t.sol
+++ packages/protocol/test/forking/gnosis/AgaveGnosis.t.sol
@@ -5,7 +5,6 @@ import "forge-std/console.sol";
 import {Routines} from "../../utils/Routines.sol";
 import {ForkingSetup} from "../ForkingSetup.sol";
 import {ILendingProvider} from "../../../src/interfaces/ILendingProvider.sol";
-import {BorrowingVault} from "../../../src/vaults/borrowing/BorrowingVault.sol";
 import {AgaveGnosis} from "../../../src/providers/gnosis/AgaveGnosis.sol";
 
 contract AgaveGnosisForkingTest is Routines, ForkingSetup {
@@ -15,7 +14,11 @@ contract AgaveGnosisForkingTest is Routines, ForkingSetup {
   uint256 public constant BORROW_AMOUNT = 200 * 1e6;
 
   function setUp() public {
-    deploy(GNOSIS_DOMAIN);
+    setUpFork(GNOSIS_DOMAIN);
+
+    agave = new AgaveGnosis();
+    ILendingProvider[] memory providers = new ILendingProvider[](1);
+    providers[0] = agave;
 
     deployVault(
       registry[GNOSIS_DOMAIN].weth,
@@ -23,14 +26,9 @@ contract AgaveGnosisForkingTest is Routines, ForkingSetup {
       796341757142697,
       100000000,
       "WETH",
-      "DAI"
+      "DAI",
+      providers
     );
-    agave = new AgaveGnosis();
-    ILendingProvider[] memory providers = new ILendingProvider[](1);
-    providers[0] = agave;
-
-    _setVaultProviders(vault, providers);
-    vault.setActiveProvider(agave);
   }
 
   function test_depositAndBorrow() public {
