4053e4ed18466d215cb694cfe21bb9a2eaa19e2a
pedrovalido <pedro.validex@gmail.com>
2023-01-16 16:22:47 +0000

    test(protocol): get balances acrobatic

--- packages/protocol/test/forking/mainnet/IronBank.t.sol
+++ packages/protocol/test/forking/mainnet/IronBank.t.sol
@@ -5,8 +5,9 @@ import "forge-std/console.sol";
 import {Routines} from "../../utils/Routines.sol";
 import {ForkingSetup} from "../ForkingSetup.sol";
 import {ILendingProvider} from "../../../src/interfaces/ILendingProvider.sol";
-import {BorrowingVault} from "../../../src/vaults/borrowing/BorrowingVault.sol";
 import {IronBank} from "../../../src/providers/mainnet/IronBank.sol";
+import {IAddrMapper} from "../../../src/interfaces/IAddrMapper.sol";
+import {ICToken} from "../../../src/interfaces/compoundV2/ICToken.sol";
 
 contract IronBankTest is Routines, ForkingSetup {
   ILendingProvider public ironBank;
@@ -50,6 +51,35 @@ contract IronBankTest is Routines, ForkingSetup {
     //account for rounding issue
     assertApproxEqAbs(depositBalance, DEPOSIT_AMOUNT, DEPOSIT_AMOUNT / 1000);
     assertApproxEqAbs(borrowBalance, BORROW_AMOUNT, BORROW_AMOUNT / 1000);
+  }
+
+  function test_getBalancesAcrobatic(uint256 nBlocks) public {
+    vm.assume(nBlocks > 0 && nBlocks < 100000000);
+    do_depositAndBorrow(DEPOSIT_AMOUNT, BORROW_AMOUNT, vault, ALICE);
+
+    uint256 borrowBalance = vault.totalDebt();
+
+    uint256 borrowBalance1 = ICToken(
+      IAddrMapper(0x529eE84BFE4F37132f5f9599d4cc4Ff16Ee6d0D2).getAddressMapping(
+        "IronBank", debtAsset
+      )
+    ).borrowBalanceCurrent(address(vault));
+    assertEq(borrowBalance, borrowBalance1);
+
+    //check after few blocks
+    uint256 time = 2 * nBlocks;
+    vm.warp(block.timestamp + time);
+    vm.roll(block.number + nBlocks);
+
+    borrowBalance = vault.totalDebt();
+
+    borrowBalance1 = ICToken(
+      IAddrMapper(0x529eE84BFE4F37132f5f9599d4cc4Ff16Ee6d0D2).getAddressMapping(
+        "IronBank", debtAsset
+      )
+    ).borrowBalanceCurrent(address(vault));
+
+    assertEq(borrowBalance, borrowBalance1);
   }
 
   function test_getInterestRates() public {
