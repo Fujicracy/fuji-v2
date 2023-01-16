df8290ce27d601d42e9bdf3e15489d669030a577
pedrovalido <pedro.validex@gmail.com>
2023-01-16 17:15:10 +0000

    fix(protocol): clear variable names

--- packages/protocol/src/libraries/LibIronBankOptimism.sol
+++ packages/protocol/src/libraries/LibIronBankOptimism.sol
@@ -24,9 +24,9 @@ library LibIronBankOptimism {
   }
 
   function viewExchangeRate(ICToken cToken) internal view returns (uint256) {
-    uint256 accrualBlockNumberPrior = cToken.accrualBlockNumber();
+    uint256 accrualBlockTimestampPrior = cToken.accrualBlockNumber(); //ironbank on optimism returns timestamp instead of block.number
 
-    if (accrualBlockNumberPrior == block.timestamp) return cToken.exchangeRateStored();
+    if (accrualBlockTimestampPrior == block.timestamp) return cToken.exchangeRateStored();
 
     uint256 totalCash = cToken.getCash();
     uint256 borrowsPrior = cToken.totalBorrows();
@@ -37,7 +37,7 @@ library LibIronBankOptimism {
     require(borrowRateMantissa <= 0.0005e16, "RATE_TOO_HIGH"); // Same as borrowRateMaxMantissa in ICTokenInterfaces.sol
 
     uint256 interestAccumulated =
-      (borrowRateMantissa * (block.timestamp - accrualBlockNumberPrior)).mulWadDown(borrowsPrior);
+      (borrowRateMantissa * (block.timestamp - accrualBlockTimestampPrior)).mulWadDown(borrowsPrior);
 
     uint256 totalReserves =
       cToken.reserveFactorMantissa().mulWadDown(interestAccumulated) + reservesPrior;
@@ -50,14 +50,14 @@ library LibIronBankOptimism {
 
   function viewNewBorrowIndex(ICToken cToken) internal view returns (uint256 newBorrowIndex) {
     /* Remember the initial block timestamp */
-    uint256 currentBlockNumber = block.timestamp;
-    uint256 accrualBlockNumberPrior = cToken.accrualBlockNumber();
+    uint256 currentBlockTimestamp = block.timestamp;
+    uint256 accrualBlockTimestampPrior = cToken.accrualBlockNumber(); //ironbank on optimism returns timestamp instead of block.number
 
     /* Read the previous values out of storage */
     uint256 borrowIndexPrior = cToken.borrowIndex();
 
     /* Short-circuit accumulating 0 interest */
-    if (accrualBlockNumberPrior == currentBlockNumber) {
+    if (accrualBlockTimestampPrior == currentBlockTimestamp) {
       newBorrowIndex = borrowIndexPrior;
     }
 
@@ -66,7 +66,7 @@ library LibIronBankOptimism {
     require(borrowRateMantissa <= 0.0005e16, "RATE_TOO_HIGH"); // Same as borrowRateMaxMantissa in ICTokenInterfaces.sol
 
     /* Calculate the number of blocks elapsed since the last accrual */
-    uint256 blockDelta = currentBlockNumber - accrualBlockNumberPrior;
+    uint256 blockDelta = currentBlockTimestamp - accrualBlockTimestampPrior;
 
     uint256 simpleInterestFactor = borrowRateMantissa * blockDelta;
     newBorrowIndex = (simpleInterestFactor * borrowIndexPrior) / 1e18 + borrowIndexPrior;
