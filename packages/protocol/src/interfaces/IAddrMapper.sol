26d582b1975cfd7cc7ae15ae272f1bdf55a63262
pedrovalido <pedro.validex@gmail.com>
2023-01-24 00:15:42 +0100

    docs(protocol): iaddrmapper comments

--- packages/protocol/src/interfaces/IAddrMapper.sol
+++ packages/protocol/src/interfaces/IAddrMapper.sol
@@ -3,10 +3,15 @@ pragma solidity 0.8.15;
 
 interface IAddrMapper {
   /**
-   * @dev Log a change in address mapping
+   * @notice Log a change in address mapping
    */
   event MappingChanged(address[] keyAddress, address mappedAddress);
 
+  /**
+   * @notice Returns the address of the underlying token associated with the keyAddr for the providerName protocol
+   * @param providerName string name of the provider
+   * @param keyAddr address of the token associated with the underlying token
+   */
   function getAddressMapping(
     string memory providerName,
     address keyAddr
@@ -15,6 +20,12 @@ interface IAddrMapper {
     view
     returns (address returnedAddr);
 
+  /**
+   * @notice Returns the address of the underlying token associated with both keyAddr1 and keyAddr2 tokens
+   * @param providerName string name of the provider
+   * @param keyAddr1 address of the token (provided as collateral) associated with the underlying token
+   * @param keyAddr2 address of the token (borrowed) associated with the underlying token
+   */
   function getAddressNestedMapping(
     string memory providerName,
     address keyAddr1,
@@ -24,8 +35,21 @@ interface IAddrMapper {
     view
     returns (address returnedAddr);
 
+  /**
+   * @notice Sets the mapping of the underlying (returnedAddr) token associated with the providerName and the token (keyAddr)
+   * @param providerName string name of the provider
+   * @param keyAddr address of the token associated with the underlying token
+   * @param returnedAddr address of the underlying token to be returned by the getAddressMapping
+   */
   function setMapping(string memory providerName, address keyAddr, address returnedAddr) external;
 
+  /**
+   * @notice Sets the mapping associated with the providerName and both keyAddr1 (collateral) and keyAddr2 (borrowed) tokens
+   * @param providerName string name of the provider
+   * @param keyAddr1 address of the token provided as collateral
+   * @param keyAddr2 address of the token to be borrowed
+   * @param returnedAddr address of the underlying token to be returned by the getAddressNestedMapping
+   */
   function setNestedMapping(
     string memory providerName,
     address keyAddr1,
