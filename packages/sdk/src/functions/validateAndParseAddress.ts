c419e56c379bede1a494780f6048858ca6aecb6f
Boyan Barakov <boyan.barakov@gmail.com>
2023-01-24 08:00:57 +0100

    fix(sdk): connext sdk version; tiny-invariant

--- packages/sdk/src/functions/validateAndParseAddress.ts
+++ packages/sdk/src/functions/validateAndParseAddress.ts
@@ -1,12 +1,13 @@
 import { getAddress } from '@ethersproject/address';
 import invariant from 'tiny-invariant';
-import warning from 'tiny-warning';
 
 // warns if addresses are not checksummed
 export function validateAndParseAddress(address: string): string {
   try {
     const checksummedAddress = getAddress(address);
-    warning(address === checksummedAddress, `${address} is not checksummed.`);
+    if (address !== checksummedAddress)
+      console.warn(`${address} is not checksummed.`);
+    //warning(address === checksummedAddress, `${address} is not checksummed.`);
     return checksummedAddress;
   } catch (error) {
     invariant(false, `${address} is not a valid address.`);
