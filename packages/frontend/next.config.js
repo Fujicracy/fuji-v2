d598d32858c8117fb3006bf31bf27efd9ad8d8d9
Guillaume Lodi <guillaume.lodi@gmail.com>
2023-02-05 20:42:21 +0100

    fix(frontend): style

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -12,11 +12,11 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
-        // by next.js will be dropped. Doesn't make much sense, but how it is
+      // by next.js will be dropped. Doesn't make much sense, but how it is
       fs: false, // the solution
-    };
+    }
 
-    return config;
+    return config
   },
 }
 
