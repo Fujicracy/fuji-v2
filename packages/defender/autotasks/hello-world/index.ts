b5b9367f850bafc15b5e967b3d3b0f2b05f1114a
Boyan Barakov <boyan.barakov@gmail.com>
2023-02-22 17:14:38 +0100

    chore(defender): add sdk as a package and tsconfig

--- packages/defender/autotasks/hello-world/index.ts
+++ packages/defender/autotasks/hello-world/index.ts
@@ -1,4 +1,9 @@
-export async function handler(event) {
+import { AutotaskEvent } from 'defender-autotask-utils';
+import { Sdk } from '@x-fuji/sdk';
+
+export async function handler(event: AutotaskEvent) {
+  const sdk = new Sdk({ infuraId: '9696141dd20c48039f66168e505bc3c7', alchemy: {} });
+  console.log(sdk);
   console.log(`Hello world from serverless`);
   console.log(JSON.stringify(event));
   return `Hello world from serverless`;
