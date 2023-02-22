44186f59468e4cd3a2d242d3313be24d9cdca534
Dinh Quoc Trung <trungore@Dinhs-MacBook-Air.local>
2023-02-22 16:14:08 +0700

    add test for comp tokens

--- packages/protocol/test/forking/mainnet/CompoundV2.t.sol
+++ packages/protocol/test/forking/mainnet/CompoundV2.t.sol
@@ -8,6 +8,8 @@ import {CompoundV2} from "../../../src/providers/mainnet/CompoundV2.sol";
 import {ILendingProvider} from "../../../src/interfaces/ILendingProvider.sol";
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
+import {IComptroller} from "../../../src/interfaces/compoundV2/IComptroller.sol";
+
 contract CompoundV2ForkingTest is Routines, ForkingSetup {
   ILendingProvider public compoundV2;
 
@@ -22,6 +24,38 @@ contract CompoundV2ForkingTest is Routines, ForkingSetup {
     providers[0] = compoundV2;
 
     deploy(providers);
+  }
+
+  function test_cannot_claim_comp() public {
+    IERC20 comp = IERC20(0xc00e94Cb662C3520282E6f5717214004A7f26888);
+    IComptroller comptroller = IComptroller(0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B);
+
+    // ALICE borrow 
+    do_deposit(100 ether, vault, ALICE);
+    do_borrow(3 * 10**10, vault, ALICE);
+
+    // a lot of time pass
+    vm.warp(block.timestamp + 1000);
+    vm.roll(block.number + 100);
+
+    // these 2 lines used to trigger the update comp index in comtroller 
+    vm.prank(ALICE);
+    vault.borrow(BORROW_AMOUNT, ALICE, ALICE);
+
+    // get accrued comp for the vault 
+    (bool ok, bytes memory data) = address(comptroller).call(
+      abi.encodeWithSignature(
+        "compAccrued(address)",
+        address(vault)
+      )
+    );
+
+    // amount of comp is bigger than 0
+    uint256 compAccrued = abi.decode(data, (uint256));
+    console.log(compAccrued);
+    assertGt(compAccrued, 0);
+
+    // there is no function to claim COMP here :((
   }
 
   function test_depositAndBorrow() public {
