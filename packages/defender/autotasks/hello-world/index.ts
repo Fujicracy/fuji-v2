9fd27e54939de4407ae7c0fedaf1d87f11ca70cc
Boyan Barakov <boyan.barakov@gmail.com>
2023-02-23 22:01:02 +0100

    chore(defender): use connext sdk

--- packages/defender/autotasks/hello-world/index.ts
+++ packages/defender/autotasks/hello-world/index.ts
@@ -1,10 +1,38 @@
-import { AutotaskEvent } from 'defender-autotask-utils';
-import { Sdk } from '@x-fuji/sdk';
+import { create, SdkConfig } from "@connext/sdk";
+import { AutotaskEvent } from "defender-autotask-utils";
+import {
+  DefenderRelayProvider,
+  DefenderRelaySigner,
+} from "defender-relay-client/lib/ethers";
 
 export async function handler(event: AutotaskEvent) {
-  const sdk = new Sdk({ infuraId: '9696141dd20c48039f66168e505bc3c7', alchemy: {} });
-  console.log(sdk);
-  console.log(`Hello world from serverless`);
+  const credentials = event.credentials as string;
+  const relayerARN = event.relayerARN as string;
+  const relayerParams = { credentials, relayerARN };
+
+  const payload = event.request?.body;
+  console.log(payload);
+  //const { matchReasons } = payload.events[0];
+  //console.log(matchReasons[0].params);
+
+  // or just create Relayer
+  const provider = new DefenderRelayProvider(relayerParams);
+  const signer = new DefenderRelaySigner(relayerParams, provider, {
+    speed: "fast",
+  });
+  const signerAddress = await signer.getAddress();
+  const sdkConfig: SdkConfig = {
+    signerAddress,
+    network: "mainnet",
+    chains: {
+      1886350457: {
+        providers: [provider.connection.url],
+      },
+    },
+  };
+  const { sdkBase } = await create(sdkConfig);
+  console.log(sdkBase);
+
   console.log(JSON.stringify(event));
   return `Hello world from serverless`;
-};
+}
