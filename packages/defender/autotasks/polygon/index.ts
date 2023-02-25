6ed561166360a4035b7dd48f0455647783480db4
Boyan Barakov <boyan.barakov@gmail.com>
2023-02-25 11:13:08 +0100

    chore(defender): init tx with relayer; comment out sentinels in serverless

--- packages/defender/autotasks/polygon/index.ts
+++ packages/defender/autotasks/polygon/index.ts
@@ -1,27 +1,52 @@
+import axios from "axios";
 import { AutotaskEvent } from "defender-autotask-utils";
-//import {
-  //DefenderRelayProvider,
-  //DefenderRelaySigner,
-//} from "defender-relay-client/lib/ethers";
+import {
+  DefenderRelayProvider,
+  DefenderRelaySigner,
+} from "defender-relay-client/lib/ethers";
+import { ethers } from "ethers";
+
+const POLGYGON_DOMAIN = "1886350457";
+const ADDRESS = "0x403b1E6EFB00C440dDb60593255c1257f4156863";
+const ABI = [{
+  "inputs": [
+    {
+      "internalType": "bytes32",
+      "name": "transferId",
+      "type": "bytes32"
+    }
+  ],
+  "name": "bumpTransfer",
+  "outputs": [],
+  "stateMutability": "payable",
+  "type": "function"
+}];
 
 export async function handler(event: AutotaskEvent) {
   const credentials = event.credentials as string;
   const relayerARN = event.relayerARN as string;
   const relayerParams = { credentials, relayerARN };
-  console.log(relayerParams);
 
-  const payload = event.request?.body;
-  console.log(payload);
-  //const { matchReasons } = payload.events[0];
-  //console.log(matchReasons[0].params);
+  const payload = event.request?.body as any;
+  const { matchReasons } = payload.events[0];
+  console.log(matchReasons[0].params);
+
+  const params = matchReasons[0].params;
+  const destinationDomain = params.destDomain;
+  const transferId = params.transferId;
+
+  const { data } = await axios.get(
+    `https://connext-estimate-fees-function-gususkd65q-uc.a.run.app/estimateRelayerFee?originDomain=${POLGYGON_DOMAIN}&destinationDomain=${destinationDomain}`
+  );
 
   // or just create Relayer
-  //const provider = new DefenderRelayProvider(relayerParams);
-  //const signer = new DefenderRelaySigner(relayerParams, provider, {
-    //speed: "fast",
-  //});
-  //console.log(signer);
+  const provider = new DefenderRelayProvider(relayerParams);
+  const signer = new DefenderRelaySigner(relayerParams, provider, {
+    speed: "fast",
+  });
+  const contract = new ethers.Contract(ADDRESS, ABI, signer);
+  const tx = await contract.bumpTransfer(transferId, { value: data })
 
-  console.log(JSON.stringify(event));
-  return `Hello world from serverless`;
+  //console.log(JSON.stringify(event));
+  return tx;
 }
