99e3f46b1aad4a22a2b7f8de45b60b41f499b5cb
pedrovalido <pedro.validex@gmail.com>
2023-03-15 16:26:39 +0800

    test(protocol): denial of service borrowing vault more use cases

--- packages/protocol/test/forking/mainnet/DenialOfService.t.sol
+++ packages/protocol/test/forking/mainnet/DenialOfService.t.sol
@@ -38,7 +38,7 @@ contract DenialOfServiceTest is Routines, ForkingSetup {
     aaveV2pool = IV2Pool(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9);
   }
 
-  function test_denialOfService() public {
+  function test_denialOfServiceWithdraw() public {
     // Alice deposits and borrows
     do_deposit(DEPOSIT_AMOUNT, vault, ALICE);
     do_borrow(BORROW_AMOUNT, vault, ALICE);
@@ -51,10 +51,80 @@ contract DenialOfServiceTest is Routines, ForkingSetup {
     vm.stopPrank();
 
     assertEq(vault.balanceOf(ALICE), DEPOSIT_AMOUNT);
+    assertEq(vault.balanceOfDebt(ALICE), 0);
 
     uint256 maxAmount = vault.maxWithdraw(ALICE);
     do_withdraw(maxAmount, vault, ALICE);
 
     assertApproxEqAbs(vault.balanceOf(ALICE), 0, 1);
+  }
+
+  function test_denialOfServiceWithdrawMoreUsers() public {
+    do_deposit(DEPOSIT_AMOUNT, vault, ALICE);
+    do_borrow(BORROW_AMOUNT, vault, ALICE);
+    do_deposit(DEPOSIT_AMOUNT, vault, BOB);
+    do_borrow(BORROW_AMOUNT, vault, BOB);
+
+    // Troublemaker pays vaults debt
+    deal(debtAsset, TROUBLEMAKER, BORROW_AMOUNT * 10);
+    vm.startPrank(TROUBLEMAKER);
+    IERC20(debtAsset).safeApprove(address(aaveV2pool), BORROW_AMOUNT);
+    aaveV2pool.repay(vault.debtAsset(), BORROW_AMOUNT * 2, 2, address(vault));
+    vm.stopPrank();
+
+    assertEq(vault.balanceOf(ALICE), DEPOSIT_AMOUNT);
+    assertEq(vault.balanceOfDebt(ALICE), 0);
+    assertEq(vault.balanceOf(BOB), DEPOSIT_AMOUNT);
+    assertEq(vault.balanceOfDebt(BOB), 0);
+
+    uint256 maxAmount = vault.maxWithdraw(ALICE);
+    do_withdraw(maxAmount, vault, ALICE);
+    maxAmount = vault.maxWithdraw(BOB);
+    do_withdraw(maxAmount, vault, BOB);
+
+    assertApproxEqAbs(vault.balanceOf(ALICE), 0, 1);
+    assertApproxEqAbs(vault.balanceOf(BOB), 0, 1);
+  }
+
+  function test_denialOfServiceBorrowPaybackWithdraw() public {
+    do_deposit(DEPOSIT_AMOUNT, vault, ALICE);
+    do_borrow(BORROW_AMOUNT, vault, ALICE);
+    do_deposit(DEPOSIT_AMOUNT, vault, BOB);
+    do_borrow(BORROW_AMOUNT, vault, BOB);
+
+    // Troublemaker pays vaults debt
+    deal(debtAsset, TROUBLEMAKER, BORROW_AMOUNT * 10);
+    vm.startPrank(TROUBLEMAKER);
+    IERC20(debtAsset).safeApprove(address(aaveV2pool), BORROW_AMOUNT);
+    aaveV2pool.repay(vault.debtAsset(), BORROW_AMOUNT, 2, address(vault));
+    vm.stopPrank();
+
+    assertEq(vault.balanceOf(ALICE), DEPOSIT_AMOUNT);
+    assertEq(vault.balanceOfDebt(ALICE), BORROW_AMOUNT / 2);
+    assertEq(vault.balanceOf(BOB), DEPOSIT_AMOUNT);
+    assertEq(vault.balanceOfDebt(BOB), BORROW_AMOUNT / 2);
+
+    //Debt should be less so user can borrow more
+    do_borrow(BORROW_AMOUNT / 2, vault, ALICE);
+    do_borrow(BORROW_AMOUNT / 2, vault, BOB);
+
+    assertEq(vault.balanceOfDebt(ALICE), BORROW_AMOUNT);
+    assertEq(vault.balanceOfDebt(BOB), BORROW_AMOUNT);
+
+    //payback debt
+    do_payback(BORROW_AMOUNT, vault, ALICE);
+    do_payback(BORROW_AMOUNT, vault, BOB);
+
+    assertEq(vault.balanceOfDebt(ALICE), 0);
+    assertEq(vault.balanceOfDebt(BOB), 0);
+
+    //withdraw
+    uint256 maxAmount = vault.maxWithdraw(ALICE);
+    do_withdraw(maxAmount, vault, ALICE);
+    maxAmount = vault.maxWithdraw(BOB);
+    do_withdraw(maxAmount, vault, BOB);
+
+    assertApproxEqAbs(vault.balanceOf(ALICE), 0, 1);
+    assertApproxEqAbs(vault.balanceOf(BOB), 0, 1);
   }
 }
