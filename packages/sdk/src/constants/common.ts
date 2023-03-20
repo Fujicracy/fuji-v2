9134e99918991f9dd40a9ae60d8cffb447441e10
Federico Erostarbe <erostarbe@gmail.com>
2023-03-20 17:07:21 -0300

    refactor(sdk): constant urls

--- packages/sdk/src/constants/common.ts
+++ packages/sdk/src/constants/common.ts
@@ -1,1 +1,12 @@
+// Default values
 export const DEFAULT_SLIPPAGE = 30;
+
+// Urls
+export const URLS = {
+  // DefiLlama
+  DEFILLAMA_LEND_BORROW: 'https://yields.llama.fi/lendBorrow',
+  DEFILLAMA_POOLS: 'https://yields.llama.fi/pools',
+};
+
+export const CONNEXT_URL = (chain: string, transfer: string): string =>
+  `https://postgrest.${chain}.connext.ninja/transfers?transfer_id=eq.${transfer}&select=status,execute_transaction_hash`;
