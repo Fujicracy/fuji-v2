d9666e5c155e6779058e05aae950fb7cd0486697
Federico Erostarbe <erostarbe@gmail.com>
2023-03-22 19:12:18 -0300

    fix(frontend): prettier fix

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -3,18 +3,18 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: "/",
-        destination: "/markets",
+        source: '/',
+        destination: '/markets',
         permanent: true,
       },
-    ]
+    ];
   },
   reactStrictMode: true,
   swcMinify: true,
   rewrites: () => [
     {
-      source: "/proxy/defillama/:path",
-      destination: "https://yields.llama.fi/:path",
+      source: '/proxy/defillama/:path',
+      destination: 'https://yields.llama.fi/:path',
     },
   ],
   // fix "Module not found: Can't resolve 'fs'" thrown by @connext/nxtp-sdk
@@ -23,10 +23,10 @@ const nextConfig = {
       ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
       // by next.js will be dropped. Doesn't make much sense, but how it is
       fs: false, // the solution
-    }
+    };
 
-    return config
+    return config;
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
