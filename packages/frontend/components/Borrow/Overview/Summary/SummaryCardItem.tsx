0735a5b48067e5be6e24a590b315a394d37cdb7b
Federico Erostarbe <erostarbe@gmail.com>
2023-03-27 16:00:46 -0300

    fix(frontend): debt amount -mobile

--- packages/frontend/components/Borrow/Overview/Summary/SummaryCardItem.tsx
+++ packages/frontend/components/Borrow/Overview/Summary/SummaryCardItem.tsx
@@ -24,11 +24,10 @@ function SummaryCardItem({
   const { palette } = useTheme();
 
   if (isMobile) {
-    const shouldHaveParenthesis =
-      title !== 'Current Price' && title !== 'Borrowed Value';
-    const content = `${amount} ${shouldHaveParenthesis ? '(' : ''}${
-      title !== 'Borrowed Value' ? footer : ''
-    }${shouldHaveParenthesis ? ')' : ''}`;
+    const shouldHaveParenthesis = title !== 'Current Price';
+    const content = `${amount} ${shouldHaveParenthesis ? '(' : ''}${footer}${
+      shouldHaveParenthesis ? ')' : ''
+    }`;
 
     return (
       <Grid item sx={{ display: 'flex', justifyContent: 'space-between' }}>
