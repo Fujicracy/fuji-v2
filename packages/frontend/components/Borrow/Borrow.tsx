e8a2de5a448cb4231f3bedae6a91517c81dff14e
Federico Erostarbe <erostarbe@gmail.com>
2023-03-30 16:31:02 -0300

    fix(frontend): reverted show ltv change

--- packages/frontend/components/Borrow/Borrow.tsx
+++ packages/frontend/components/Borrow/Borrow.tsx
@@ -163,8 +163,7 @@ function Borrow({ isEditing, basePosition }: BorrowProps) {
               type === 'debt' && debtAmount && debtAmount < balance
                 ? debtAmount
                 : balance;
-            const showLtv =
-              type === 'debt' && actionType === ActionType.ADD && !isEditing;
+            const showLtv = type === 'debt' && actionType === ActionType.ADD;
             return (
               <BorrowBox
                 key={type}
