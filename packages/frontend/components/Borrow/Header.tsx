667b7c604dbef5040aef29f0946119112c7980d0
NickolaiYurchenko <nickolai.yurchenko@gmail.com>
2023-04-06 14:29:01 +0300

    feat(frontend): poosition header changed

--- packages/frontend/components/Borrow/Header.tsx
+++ packages/frontend/components/Borrow/Header.tsx
@@ -1,7 +1,9 @@
 import { Box, Divider, Stack, Typography } from '@mui/material';
 
 import { ActionType } from '../../helpers/assets';
+import { useBorrow } from '../../store/borrow.store';
 import { NetworkIcon } from '../Shared/Icons';
+import { TokenIcon } from '../Shared/Icons';
 import SlippageSettings from '../Shared/SlippageSettings';
 import TabChip from '../Shared/TabChip';
 import TooltipWrapper from '../Shared/Tooltips/TooltipWrapper';
@@ -22,6 +24,8 @@ function BorrowHeader({
   isCrossChainOperation,
 }: BorrowHeaderProps) {
   const networkMessage = `Your position is currently on the ${chainName} Network`;
+  const collateral = useBorrow((state) => state.collateral);
+  const debt = useBorrow((state) => state.debt);
 
   return (
     <>
@@ -32,9 +36,35 @@ function BorrowHeader({
           alignItems="center"
           height="40px"
         >
-          <Typography variant="body2" height="40px" lineHeight="40px">
-            Manage your position
-          </Typography>
+          <Stack direction="row" justifyContent="start" alignItems="center">
+            <Box sx={{ position: 'relative' }}>
+              <TokenIcon token={debt.token} height={40} width={40} />
+              <TokenIcon
+                token={collateral.token}
+                height={16}
+                width={16}
+                sx={{
+                  position: 'absolute',
+                  botton: 0,
+                  right: 0,
+                  transform: 'translateY(-100%)',
+                }}
+              />
+            </Box>
+            <Box ml="0.75rem">
+              <Typography variant="h5" fontSize="1.25rem" lineHeight="150%">
+                Debt: {debt.token.symbol}
+              </Typography>
+              <Typography
+                variant="small"
+                fontSize="0.875rem"
+                lineHeight="22.4px"
+              >
+                Collateral: {collateral.token.symbol}
+              </Typography>
+            </Box>
+          </Stack>
+
           <Stack
             direction="row"
             justifyContent="space-between"
