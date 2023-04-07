8135a90e8898fb8b03149f5faf2ec88a8cbd72cf
NickolaiYurchenko <nickolai.yurchenko@gmail.com>
2023-04-07 09:28:32 +0300

    fix(frontend): network section removed

--- packages/frontend/components/Shared/ConfirmTransactionModal.tsx
+++ packages/frontend/components/Shared/ConfirmTransactionModal.tsx
@@ -326,34 +326,6 @@ function AssetBox({
           </Typography>
         </Stack>
       </Stack>
-
-      <Divider sx={{ m: '0.75rem 0', height: '1px', width: '100%' }} />
-
-      <Stack
-        width="100%"
-        flexDirection="row"
-        alignItems="center"
-        justifyContent="space-between"
-      >
-        <Typography variant="small">Network</Typography>
-
-        <Stack flexDirection="row" alignItems="center" gap={0.75}>
-          <NetworkIcon network={token.chainId} height={16} width={16} />
-          <Typography variant="small">{chainName(token.chainId)}</Typography>
-        </Stack>
-      </Stack>
-
-      <Typography
-        textAlign="start"
-        mt=".5rem"
-        variant="xsmall"
-        fontSize="0.75rem"
-        lineHeight="1.2rem"
-        color={palette.info.main}
-        sx={{ width: '60%' }}
-      >
-        The designated network where your debt position will be on.
-      </Typography>
     </Card>
   );
 }
