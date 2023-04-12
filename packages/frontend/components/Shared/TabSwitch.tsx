d520cdd895845f8da90785508f955ecbcc71c06d
NickolaiYurchenko <nickolai.yurchenko@gmail.com>
2023-04-12 14:44:10 +0300

    feat(frontend): a TabSwitch component added

---
+++ packages/frontend/components/Shared/TabSwitch.tsx
@@ -0,0 +1,50 @@
+import { Stack } from '@mui/material';
+import { alpha, useTheme } from '@mui/material/styles';
+
+import TabChip from '../Shared/TabChip';
+
+type TabSwitchProps = {
+  actions: { value: number; label: string }[];
+  selected: number;
+  onChange: (value: number) => void;
+  size?: 'large' | 'default';
+};
+
+function TabSwitch({
+  actions,
+  selected,
+  onChange,
+  size = 'default',
+}: TabSwitchProps) {
+  const { palette } = useTheme();
+
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        marginTop: 3,
+        marginBottom: 3,
+        flexWrap: 'wrap',
+        gap: '0.2rem',
+        p: '0.1875rem',
+        height: size === 'large' ? '2.875rem' : '2rem',
+        backgroundColor: palette.secondary.dark,
+        borderRadius: '0.75rem',
+        border: `1px solid ${alpha(palette.secondary.light, 0.5)}`,
+      }}
+    >
+      {actions.map((p) => (
+        <TabChip
+          key={`${p}`}
+          selected={selected === p.value}
+          label={p.label}
+          onClick={() => {
+            onChange(p.value);
+          }}
+        />
+      ))}
+    </Stack>
+  );
+}
+
+export default TabSwitch;
