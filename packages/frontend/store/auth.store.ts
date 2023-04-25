5d2f94293d3334a7af926e9f14e352ee0937b96b
Federico Erostarbe <erostarbe@gmail.com>
2023-04-25 17:55:59 -0300

    chore(frontend): updated web3-onboard (again)

--- packages/frontend/store/auth.store.ts
+++ packages/frontend/store/auth.store.ts
@@ -17,18 +17,22 @@ import { devtools } from 'zustand/middleware';
 import { fujiLogo } from '../constants';
 import { chainIdToHex, onboardChains } from '../helpers/chains';
 
-const qrcodeModalOptions = {
-  mobileLinks: ['rainbow', 'metamask', 'argent', 'trust', 'imtoken', 'pillar'],
-};
-
 const wcV1InitOptions: WalletConnectOptions = {
   version: 1,
-  qrcodeModalOptions,
+  qrcodeModalOptions: {
+    mobileLinks: [
+      'rainbow',
+      'metamask',
+      'argent',
+      'trust',
+      'imtoken',
+      'pillar',
+    ],
+  },
 };
 
 const wcV2InitOptions: WalletConnectOptions = {
   version: 2,
-  qrcodeModalOptions,
   projectId: `${process.env.WALLET_CONNECT_V2_KEY}`,
 };
 
