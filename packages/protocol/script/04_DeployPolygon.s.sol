fd19095251e80182ebf4d51811a0796ce6a246ac
Boyan Barakov <boyan.barakov@gmail.com>
2023-05-04 16:57:02 +0200

    chore(protocol): scripts set ratings

--- packages/protocol/script/04_DeployPolygon.s.sol
+++ packages/protocol/script/04_DeployPolygon.s.sol
@@ -102,7 +102,8 @@ contract DeployPolygon is ScriptPlus {
     /*_deployVault(address(WETH), address(USDC), "BorrowingVault-WETHUSDC");*/
     /*_deployVault(address(WETH), address(USDT), "BorrowingVault-WETHUSDT");*/
 
-    _setVaultNewProviders("BorrowingVault-WETHUSDC");
+    /*_setVaultNewProviders("BorrowingVault-WETHUSDC");*/
+    /*_setVaultNewRating("BorrowingVault-WETHUSDC", 75);*/
 
     vm.stopBroadcast();
   }
@@ -187,6 +188,13 @@ contract DeployPolygon is ScriptPlus {
     bytes memory callData = abi.encodeWithSelector(vault.setProviders.selector, providers);
     /*_scheduleWithTimelock(address(vault), callData);*/
     _executeWithTimelock(address(vault), callData);
+  }
+
+  function _setVaultNewRating(string memory vaultName, uint256 rating) internal {
+    bytes memory callData =
+      abi.encodeWithSelector(chief.setSafetyRating.selector, getAddress(vaultName), rating);
+    _scheduleWithTimelock(address(chief), callData);
+    /*_executeWithTimelock(address(chief), callData);*/
   }
 
   function _scheduleWithTimelock(address target, bytes memory callData) internal {
