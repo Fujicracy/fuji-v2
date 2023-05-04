d3844b1d7582e7f68750ae3cc6692bf8125162e9
Boyan Barakov <boyan.barakov@gmail.com>
2023-05-04 16:53:22 +0200

    fix(frontend): change guild id

--- packages/frontend/store/access.store.ts
+++ packages/frontend/store/access.store.ts
@@ -1,10 +1,11 @@
-import { guild, setProjectName } from "@guildxyz/sdk"
-import produce from "immer"
-import { create } from "zustand"
-import { useAuth } from "./auth.store"
+import { guild, setProjectName } from '@guildxyz/sdk';
+import produce from 'immer';
+import { create } from 'zustand';
 
-const FUJI_GUILD_ID = 461
-setProjectName("Fuji Finance")
+import { useAuth } from './auth.store';
+
+const FUJI_GUILD_ID = 20084;
+setProjectName('Fuji Finance');
 
 export enum AccessStatus {
   Verifying,
@@ -16,23 +17,23 @@ export enum AccessStatus {
 }
 
 type AccessState = {
-  status: AccessStatus
-  errorsCount: number
-  retriesCount: number
-}
+  status: AccessStatus;
+  errorsCount: number;
+  retriesCount: number;
+};
 
 type AccessActions = {
-  reset: () => void
-  verify: () => void
-}
+  reset: () => void;
+  verify: () => void;
+};
 
 const initialState: AccessState = {
   status: AccessStatus.NoAccess,
   errorsCount: 0,
   retriesCount: 0,
-}
+};
 
-export type AccessStore = AccessState & AccessActions
+export type AccessStore = AccessState & AccessActions;
 
 export const useAccess = create<AccessStore>()(
   // devtools(
@@ -44,50 +45,50 @@ export const useAccess = create<AccessStore>()(
         status: AccessStatus.NoAccess,
         retriesCount: 0,
         errorsCount: 0,
-      })
+      });
     },
     // check every 6 seconds if user is a member of the guild
     // so that we can detect in real time when they do become a member
     verify: async () => {
-      set({ status: AccessStatus.Verifying })
-      const addr = useAuth.getState().address
+      set({ status: AccessStatus.Verifying });
+      const addr = useAuth.getState().address;
 
       if (!addr) {
-        get().reset()
-        return
+        get().reset();
+        return;
       }
 
       try {
-        const reqs = await guild.getUserAccess(FUJI_GUILD_ID, addr as string)
+        const reqs = await guild.getUserAccess(FUJI_GUILD_ID, addr as string);
         if (reqs.find((r) => r.access)) {
-          set({ status: AccessStatus.Verified })
+          set({ status: AccessStatus.Verified });
         } else {
           set(
             produce((state: AccessState) => {
-              state.status = AccessStatus.NoAccess
-              state.retriesCount++
+              state.status = AccessStatus.NoAccess;
+              state.retriesCount++;
             })
-          )
+          );
           // retry in 6 secs
           setTimeout(() => {
-            get().verify()
-          }, 6000)
+            get().verify();
+          }, 6000);
         }
       } catch (e) {
         set(
           produce((state: AccessState) => {
             if (state.errorsCount > 4) {
-              state.status = AccessStatus.FatalError
+              state.status = AccessStatus.FatalError;
             } else {
               // increase counter and retry in 6 secs
-              state.status = AccessStatus.Error
-              state.errorsCount++
+              state.status = AccessStatus.Error;
+              state.errorsCount++;
               setTimeout(() => {
-                get().verify()
-              }, 6000)
+                get().verify();
+              }, 6000);
             }
           })
-        )
+        );
       }
     },
   })
@@ -96,4 +97,4 @@ export const useAccess = create<AccessStore>()(
   //   name: "fuji-v2/access",
   // }
   // )
-)
+);
