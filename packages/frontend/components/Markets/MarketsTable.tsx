b78fc4582a802edeabf8f1219b097ec39a274238
NickolaiYurchenko <nickolai.yurchenko@gmail.com>
2023-06-02 14:50:23 +0300

    fix(frontend): apy collateral -> supply

--- packages/frontend/components/Markets/MarketsTable.tsx
+++ packages/frontend/components/Markets/MarketsTable.tsx
@@ -106,7 +106,7 @@ function MarketsTable({ filters }: { filters: MarketFilters }) {
               </Stack>
             </SizableTableCell>
             <SizableTableCell width="130px" align="right">
-              Collateral APY
+              Supply APY
             </SizableTableCell>
             <SizableTableCell align="right" width="130px">
               <Stack direction="row" alignItems="center" justifyContent="right">
