2bb3f54d15d6ccec9b530c8dc84db480af365869
Federico Erostarbe <erostarbe@gmail.com>
2023-06-06 11:31:29 -0300

    fix(frontend): improved mode fix

--- packages/frontend/helpers/borrow.ts
+++ packages/frontend/helpers/borrow.ts
@@ -10,16 +10,14 @@ export function modeForContext(
   collateral: number,
   debt?: number
 ): Mode {
-  if (
-    (collateral > 0 && Number(debt) > 0) ||
-    (collateral === 0 && debt === 0)
-  ) {
+  if (!isEditing || debt === undefined) return Mode.DEPOSIT_AND_BORROW;
+  if ((collateral > 0 && debt > 0) || (collateral === 0 && debt === 0)) {
     return ActionType.ADD === actionType
       ? Mode.DEPOSIT_AND_BORROW
       : Mode.PAYBACK_AND_WITHDRAW;
   } else if (collateral > 0) {
     return ActionType.ADD === actionType ? Mode.DEPOSIT : Mode.WITHDRAW;
-  } else if (Number(debt) > 0) {
+  } else if (debt > 0) {
     return ActionType.ADD === actionType ? Mode.BORROW : Mode.PAYBACK;
   }
   return Mode.DEPOSIT_AND_BORROW;
@@ -30,7 +28,6 @@ export const failureForMode = (
   collateral?: string,
   debt?: string
 ): boolean => {
-  console.log('HELLo');
   return (
     ((mode === Mode.DEPOSIT_AND_BORROW || mode === Mode.PAYBACK_AND_WITHDRAW) &&
       (!collateral || !debt)) ||
