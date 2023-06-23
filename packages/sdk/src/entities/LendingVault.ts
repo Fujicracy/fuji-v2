9b62229cfbd25e63ef1b8c8cca2b61e63bb80b79
Federico Erostarbe <erostarbe@gmail.com>
2023-06-23 18:54:29 -0300

    feat(sdk): added mock lending vault

---
+++ packages/sdk/src/entities/LendingVault.ts
@@ -0,0 +1,31 @@
+import { VaultType } from '../enums';
+import { ChainConfig, LendingProviderWithFinancials } from '../types';
+import { AbstractVault, AccountBalances } from './abstract/AbstractVault';
+import { Address } from './Address';
+import { Token } from './Token';
+
+/**
+ * The LendingVault class encapsulates the end-user logic of interaction with the
+ * YieldVault contract without the need to deal directly with ethers.js (ABIs, providers etc).
+ *
+ * It contains read-only functions and leaves to the client only the final step of a blockchain write.
+ * The class aims to expose functions that together with user's inputs go throughout the most common
+ * path of interacting with a YieldVault contract.
+ */
+export class LendingVault extends AbstractVault {
+  constructor(address: Address, collateral: Token) {
+    super(address, collateral, VaultType.LEND);
+  }
+  preLoad(): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+  getProviders(): Promise<LendingProviderWithFinancials[]> {
+    throw new Error('Method not implemented.');
+  }
+  setConnection(configParams: ChainConfig): AbstractVault {
+    throw new Error('Method not implemented.');
+  }
+  getBalances(account: Address): Promise<AccountBalances> {
+    throw new Error('Method not implemented.');
+  }
+}
