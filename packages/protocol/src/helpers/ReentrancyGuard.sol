95da04d2bf26fbcbf61dbf62bf91affb793385fc
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-06-24 07:42:15 -0700

    chore(protocol): modifiy oz reentrancy guard to use in other fuji cases

--- packages/protocol/src/helpers/ReentrancyGuard.sol
+++ packages/protocol/src/helpers/ReentrancyGuard.sol
@@ -1,25 +1,23 @@
-// SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v4.9.0) (security/ReentrancyGuard.sol)
-
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.15;
 
 /**
- * @dev Contract module that helps prevent reentrant calls to a function.
+ * @title ReentrancyGuard
  *
- * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
- * available, which can be applied to functions to make sure there are no nested
- * (reentrant) calls to them.
+ * @author Fujidao Labs
  *
- * Note that because there is a single `nonReentrant` guard, functions marked as
- * `nonReentrant` may not call one another. This can be worked around by making
- * those functions `private`, and then adding `external` `nonReentrant` entry
- * points to them.
+ * @notice This contract implementats a slight modified version of ReentrancyGuard in
+ * OpenZeppelin Contracts (v4.9.0) (security/ReentrancyGuard.sol).
  *
- * TIP: If you would like to learn more about reentrancy and alternative ways
- * to protect against it, check out our blog post
- * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
+ * @dev Changes include:
+ * - Definition of error `ReentrancyGuard_reentrantCall()`.
+ * - Constants `_NOT_ENTERED` and `_ENTERED` are declared as 'internal'.
+ * - Usage of revert error statement in `_nonReentrantBefore()`. *
  */
 abstract contract ReentrancyGuard {
+  /// Custom Errors
+  error ReentrancyGuard_reentrantCall();
+
   // Booleans are more expensive than uint256 or any type that takes up a full
   // word because each write operation emits an extra SLOAD to first read the
   // slot's contents, replace the bits taken up by the boolean, and then write
@@ -31,8 +29,8 @@ abstract contract ReentrancyGuard {
   // amount. Since refunds are capped to a percentage of the total
   // transaction's gas, it is best to keep them low in cases like this one, to
   // increase the likelihood of the full refund coming into effect.
-  uint256 private constant _NOT_ENTERED = 1;
-  uint256 private constant _ENTERED = 2;
+  uint256 internal constant _NOT_ENTERED = 1;
+  uint256 internal constant _ENTERED = 2;
 
   uint256 private _status;
 
@@ -55,7 +53,9 @@ abstract contract ReentrancyGuard {
 
   function _nonReentrantBefore() private {
     // On the first call to nonReentrant, _status will be _NOT_ENTERED
-    require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+    if (_status == _ENTERED) {
+      revert ReentrancyGuard_reentrantCall();
+    }
 
     // Any calls to nonReentrant after this point will fail
     _status = _ENTERED;
