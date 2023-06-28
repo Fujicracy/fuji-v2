be291ab9929a42f8ecc0941155907b7be174d06d
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-06-27 21:26:04 -0700

    feat(protocol): add xreceiveproxy contract for safer asset transfer pattern

---
+++ packages/protocol/src/routers/XReceiveProxy.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.15;
+
+/**
+ * @title XReceiveProxy
+ *
+ * @author Fujidao Labs
+ *
+ * @notice A proxy contract that receive the XReceive call from connext to
+ * allow assets to be safely pulled in {ConnextRouter.sol}
+ */
+
+import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {IXReceiver} from "../interfaces/connext/IConnext.sol";
+import {ConnextRouter} from "./ConnextRouter.sol";
+
+contract XReceiveProxy is IXReceiver {
+  using SafeERC20 for IERC20;
+
+  error XReceiveProxy__xReceive_noValueTransfer();
+  error XReceiveProxy__xReceive_notReceivedAssetBalance();
+
+  ConnextRouter public immutable connextRouter;
+
+  /**
+   * @notice Constructor that initializes XReceiveProxy
+   */
+  constructor(address connextRouter_) {
+    // No need to check address(0) since this contract is deployed within {ConnextRouter.sol}
+    connextRouter = ConnextRouter(payable(connextRouter_));
+  }
+
+  function xReceive(
+    bytes32 transferId,
+    uint256 amount,
+    address asset,
+    address originSender,
+    uint32 originDomain,
+    bytes memory callData
+  )
+    external
+    returns (bytes memory)
+  {
+    IERC20 asset_ = IERC20(asset);
+
+    if (amount == 0) {
+      revert XReceiveProxy__xReceive_noValueTransfer();
+    }
+
+    uint256 balance = asset_.balanceOf(address(this));
+    if (balance < amount) {
+      revert XReceiveProxy__xReceive_notReceivedAssetBalance();
+    }
+    asset_.safeIncreaseAllowance(address(connextRouter), amount);
+
+    return connextRouter.xReceive(transferId, amount, asset, originSender, originDomain, callData);
+  }
+}
