76edfbd3b36a8eed7b0cde597bc129181d355012
NickolaiYurchenko <nickolai.yurchenko@gmail.com>
2023-07-03 16:34:20 +0300

    fix(frontend): aftermerge fixes

--- packages/frontend/components/Positions/MyPositionsTable.tsx
+++ packages/frontend/components/Positions/MyPositionsTable.tsx
@@ -138,7 +138,7 @@ function MyPositionsTable({ type, positions, markets }: MyPositionsTableProps) {
                     {chainName(row.chainId)}
                   </Stack>
                 </TableCell>
-                {!isLend && row.debt && (
+                {!isLend && (
                   <TableCell>
                     {row.debt && (
                       <Stack direction="row" alignItems="center">
