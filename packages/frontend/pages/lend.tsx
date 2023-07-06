6909f29056cb1dbfd55b42b64736a53044cea4ac
NickolaiYurchenko <nickolai.yurchenko@gmail.com>
2023-07-06 16:01:23 +0300

    fix(frontend): reset to create lend

--- packages/frontend/pages/lend.tsx
+++ packages/frontend/pages/lend.tsx
@@ -1,9 +1,19 @@
 import { NextPage } from 'next';
+import { useEffect } from 'react';
 
 import LendingWrapper from '../components/Lending/Wrapper';
+import { useLend } from '../store/lend.store';
 import { FormType } from '../store/types/state';
 
+const formType = FormType.Create;
+
 const LendingPage: NextPage = () => {
+  const changeFormType = useLend((state) => state.changeFormType);
+
+  useEffect(() => {
+    changeFormType(formType);
+  }, [changeFormType]);
+
   return <LendingWrapper formType={FormType.Create} />;
 };
 
