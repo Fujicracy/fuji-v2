1944a0f8131403e9bb2fb900ac25f41e22b80be6
pedrovalido <pedro.validex@gmail.com>
2023-07-06 14:12:02 +0800

    feat(protocol): add claim functions

--- packages/protocol/src/interfaces/compoundV3/ICompoundV3Rewards.sol
+++ packages/protocol/src/interfaces/compoundV3/ICompoundV3Rewards.sol
@@ -24,4 +24,8 @@ interface ICompoundV3Rewards {
   function rewardConfig(address comet) external view returns (RewardConfig memory);
 
   function getRewardOwed(address comet, address account) external returns (RewardOwed memory);
+
+  function claim(address comet, address src, bool shouldAccrue) external;
+
+  function claimTo(address comet, address src, address to, bool shouldAccrue) external;
 }
