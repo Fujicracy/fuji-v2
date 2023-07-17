a7c6a4bf8ff6f5ea5c0d859bf1940928a284af3f
pedrovalido <pedro.validex@gmail.com>
2023-07-17 16:21:12 +0100

    test(protocol): test convertToCollateral strategy with swap rewards in debtAsset for collateral

--- packages/protocol/test/mocking/vaults/VaultHarvest.t.sol
+++ packages/protocol/test/mocking/vaults/VaultHarvest.t.sol
@@ -34,12 +34,115 @@ contract MockProviderIdB is MockProvider {
   }
 }
 
+contract MockProviderRewardsInDebtAsset is ILendingProvider, IHarvestable {
+  function providerName() public pure virtual override returns (string memory) {
+    return "MockProviderRewardsInDebtAsset";
+  }
+
+  function approvedOperator(
+    address keyAsset,
+    address,
+    address
+  )
+    external
+    pure
+    override
+    returns (address operator)
+  {
+    operator = keyAsset;
+  }
+
+  function deposit(uint256 amount, IVault vault) external override returns (bool success) {
+    MockERC20 merc20 = MockERC20(vault.asset());
+    try merc20.makeDeposit(address(vault), amount, providerName()) returns (bool result) {
+      success = result;
+    } catch {}
+  }
+
+  function borrow(uint256 amount, IVault vault) external override returns (bool success) {
+    MockERC20 merc20 = MockERC20(vault.debtAsset());
+    try merc20.mintDebt(address(vault), amount, providerName()) returns (bool result) {
+      success = result;
+    } catch {}
+  }
+
+  function withdraw(uint256 amount, IVault vault) external override returns (bool success) {
+    MockERC20 merc20 = MockERC20(vault.asset());
+    try merc20.withdrawDeposit(address(vault), amount, providerName()) returns (bool result) {
+      success = result;
+    } catch {}
+  }
+
+  function payback(uint256 amount, IVault vault) external override returns (bool success) {
+    MockERC20 merc20 = MockERC20(vault.debtAsset());
+    try merc20.burnDebt(address(vault), amount, providerName()) returns (bool result) {
+      success = result;
+    } catch {}
+  }
+
+  function getDepositRateFor(IVault) external pure override returns (uint256 rate) {
+    rate = 1e27;
+  }
+
+  function getBorrowRateFor(IVault) external pure override returns (uint256 rate) {
+    rate = 1e27;
+  }
+
+  function getDepositBalance(
+    address user,
+    IVault vault
+  )
+    external
+    view
+    override
+    returns (uint256 balance)
+  {
+    balance = MockERC20(vault.asset()).balanceOfDeposit(user, providerName());
+  }
+
+  function getBorrowBalance(
+    address user,
+    IVault vault
+  )
+    external
+    view
+    override
+    returns (uint256 balance)
+  {
+    balance = MockERC20(vault.debtAsset()).balanceOfDebt(user, providerName());
+  }
+
+  function harvest(bytes memory data)
+    external
+    returns (address[] memory tokens, uint256[] memory amounts)
+  {
+    IVault vault = abi.decode(data, (IVault));
+    MockERC20 merc20 = MockERC20(BorrowingVault(payable(address(vault))).debtAsset());
+    try merc20.mintRewards(address(vault), 1e18, providerName()) returns (bool /* result */ ) {
+      return previewHarvest(vault);
+    } catch {}
+  }
+
+  function previewHarvest(IVault vault)
+    public
+    view
+    returns (address[] memory tokens, uint256[] memory amounts)
+  {
+    tokens = new address[](1);
+    tokens[0] = BorrowingVault(payable(address(vault))).debtAsset();
+    amounts = new uint256[](1);
+    amounts[0] = 1e18;
+  }
+}
+
 contract VaultHarvestUnitTests is MockingSetup, MockRoutines {
   BorrowingVault public bvault;
+  BorrowingVault public bvault2;
   YieldVault public yvault;
 
   ILendingProvider public mockProviderA;
   ILendingProvider public mockProviderB;
+  ILendingProvider public mockProviderRewardsInDebtAsset;
 
   uint256 DavidPkey = 0xD;
   address DAVID = vm.addr(DavidPkey);
@@ -60,12 +163,16 @@ contract VaultHarvestUnitTests is MockingSetup, MockRoutines {
 
     mockProviderA = new MockProviderIdA();
     mockProviderB = new MockProviderIdB();
+    mockProviderRewardsInDebtAsset = new MockProviderRewardsInDebtAsset();
+
     vm.label(address(mockProviderA), "ProviderA");
     vm.label(address(mockProviderB), "ProviderB");
+    vm.label(address(mockProviderRewardsInDebtAsset), "MockProviderRewardsInDebtAsset");
 
-    ILendingProvider[] memory providers = new ILendingProvider[](2);
+    ILendingProvider[] memory providers = new ILendingProvider[](3);
     providers[0] = mockProviderA;
     providers[1] = mockProviderB;
+    providers[2] = mockProviderRewardsInDebtAsset;
 
     bvault = new BorrowingVault(
       collateralAsset,
@@ -79,7 +186,24 @@ contract VaultHarvestUnitTests is MockingSetup, MockRoutines {
       DEFAULT_LIQ_RATIO
     );
 
+    providers[0] = mockProviderRewardsInDebtAsset;
+    providers[1] = mockProviderB;
+    providers[2] = mockProviderA;
+
+    bvault2 = new BorrowingVault(
+      collateralAsset,
+      debtAsset,
+      address(oracle),
+      address(chief),
+      "Fuji-V2 tWETH-tDAI BorrowingVault",
+      "fbvtWETHtDAI",
+      providers,
+      DEFAULT_MAX_LTV,
+      DEFAULT_LIQ_RATIO
+    );
+
     _initializeVault(address(bvault), INITIALIZER, initVaultShares);
+    _initializeVault(address(bvault2), INITIALIZER, initVaultShares);
 
     yvault = new YieldVault(
       collateralAsset,
@@ -107,6 +231,11 @@ contract VaultHarvestUnitTests is MockingSetup, MockRoutines {
     do_depositAndBorrow(DEPOSIT_AMOUNT, BORROW_AMOUNT, bvault, CHARLIE);
     do_depositAndBorrow(DEPOSIT_AMOUNT, BORROW_AMOUNT, bvault, DAVID);
 
+    do_depositAndBorrow(DEPOSIT_AMOUNT, BORROW_AMOUNT, bvault2, ALICE);
+    do_depositAndBorrow(DEPOSIT_AMOUNT, BORROW_AMOUNT, bvault2, BOB);
+    do_depositAndBorrow(DEPOSIT_AMOUNT, BORROW_AMOUNT, bvault2, CHARLIE);
+    do_depositAndBorrow(DEPOSIT_AMOUNT, BORROW_AMOUNT, bvault2, DAVID);
+
     do_deposit(DEPOSIT_AMOUNT, yvault, ALICE);
     do_deposit(DEPOSIT_AMOUNT, yvault, BOB);
     do_deposit(DEPOSIT_AMOUNT, yvault, CHARLIE);
@@ -127,5 +256,30 @@ contract VaultHarvestUnitTests is MockingSetup, MockRoutines {
     assertEq(
       mockProviderA.getDepositBalance(address(bvault), bvault) - balanceBefore, expectedRewards
     );
+  }
+
+  function test_harvestWithStrategy1AndSwap() public {
+    uint256 balanceBefore =
+      mockProviderRewardsInDebtAsset.getDepositBalance(address(bvault2), bvault2);
+
+    bytes memory data = abi.encode(bvault2);
+    vm.startPrank(HARVESTER);
+    harvestManager.harvest(
+      bvault2,
+      Strategy.ConvertToCollateral,
+      IHarvestable(address(mockProviderRewardsInDebtAsset)),
+      swapper,
+      data
+    );
+    vm.stopPrank();
+
+    uint256 expectedRewardsInDebtAsset = 1e18; //in debtAsset
+    uint256 expectedRewards = expectedRewardsInDebtAsset
+      * oracle.getPriceOf(collateralAsset, debtAsset, assetDecimals) / (10 ** debtDecimals); //in collateralAsset
+
+    uint256 rewardsInVault =
+      mockProviderRewardsInDebtAsset.getDepositBalance(address(bvault2), bvault2) - balanceBefore;
+
+    assertEq(rewardsInVault, expectedRewards);
   }
 }
