dceee17395f57ea27027c1fbd0d6e19b1366a614
pedrovalido <pedro.validex@gmail.com>
2023-07-24 19:14:15 +0100

    test(protocol): test strategy repayDebt and convertToCollateral

--- packages/protocol/test/forking/mainnet/CompoundV3.t.sol
+++ packages/protocol/test/forking/mainnet/CompoundV3.t.sol
@@ -9,6 +9,10 @@ import {ICompoundV3} from "../../../src/interfaces/compoundV3/ICompoundV3.sol";
 import {AaveV2} from "../../../src/providers/mainnet/AaveV2.sol";
 import {ILendingProvider} from "../../../src/interfaces/ILendingProvider.sol";
 import {IHarvestable} from "../../../src/interfaces/IHarvestable.sol";
+import {Strategy, HarvestManager} from "../../../src/HarvestManager.sol";
+import {IHarvestManager} from "../../../src/interfaces/IHarvestManager.sol";
+import {ISwapper} from "../../../src/interfaces/ISwapper.sol";
+import {MockSwapper} from "../../../src/mocks/MockSwapper.sol";
 import {IVault} from "../../../src/interfaces/IVault.sol";
 import {BorrowingVault} from "../../../src/vaults/borrowing/BorrowingVault.sol";
 import {YieldVault} from "../../../src/vaults/yields/YieldVault.sol";
@@ -19,6 +23,9 @@ import {TimelockController} from
 import {
   ICompoundV3Rewards, RewardOwed
 } from "../../../src/interfaces/compoundV3/ICompoundV3Rewards.sol";
+import {UniswapV2Swapper} from "../../../src/swappers/UniswapV2Swapper.sol";
+import {IUniswapV2Router01} from "../../../src/interfaces/uniswap/IUniswapV2Router01.sol";
+import {IWETH9} from "../../../src/abstracts/WETH9.sol";
 
 contract CompoundV3ForkingTests is Routines, ForkingSetup {
   ILendingProvider public compoundV3;
@@ -27,6 +34,14 @@ contract CompoundV3ForkingTests is Routines, ForkingSetup {
   uint256 public constant BORROW_AMOUNT = 200 * 1e6;
 
   IVault public yieldVault;
+
+  IHarvestManager public harvestManager;
+  ISwapper public swapper;
+
+  uint256 HarvesterPkey = 0xABCD123;
+  address HARVESTER = vm.addr(HarvesterPkey);
+  uint256 public constant TREASURY_PK = 0xF;
+  address public TREASURY = vm.addr(TREASURY_PK);
 
   function setUp() public {
     setUpFork(MAINNET_DOMAIN);
@@ -72,6 +87,18 @@ contract CompoundV3ForkingTests is Routines, ForkingSetup {
 
     _initializeVault(address(vault), INITIALIZER, initVaultShares);
     _initializeVault(address(yieldVault), INITIALIZER, initVaultShares);
+
+    harvestManager = new HarvestManager(address(chief), TREASURY);
+    _grantRoleChief(HARVESTER_ROLE, address(harvestManager));
+
+    executionCall = abi.encodeWithSelector(harvestManager.allowExecutor.selector, HARVESTER, true);
+    _callWithTimelock(address(harvestManager), executionCall);
+
+    address uniswapV2Router = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    swapper = new UniswapV2Swapper(IWETH9(collateralAsset), IUniswapV2Router01(uniswapV2Router));
+
+    executionCall = abi.encodeWithSelector(chief.allowSwapper.selector, address(swapper), true);
+    _callWithTimelock(address(chief), executionCall);
   }
 
   function test_depositAndBorrow() public {
@@ -184,4 +211,101 @@ contract CompoundV3ForkingTests is Routines, ForkingSetup {
   //   console.log("Reward Token: ", rewardOwed.token);
   //   console.log("Reward Amount: ", rewardOwed.owed);
   // }
+
+  // function test_harvest() public {
+  //   do_depositAndBorrow(DEPOSIT_AMOUNT, BORROW_AMOUNT, vault, ALICE);
+  //
+  //   vm.warp(block.timestamp + (13 * 1000 seconds));
+  //   vm.roll(block.number + 1000);
+  //
+  //   //Check view functions are working as expected
+  // address market = CompoundV3(address(compoundV3)).getMapper().getAddressNestedMapping(
+  //   compoundV3.providerName(), collateralAsset, debtAsset
+  // );
+  //   console.log("Market: ", market);
+  //   RewardOwed memory rewardOwed = ICompoundV3Rewards(0x1B0e765F6224C21223AeA2af16c1C46E38885a40)
+  //     .getRewardOwed(market, address(vault));
+  //
+  //   //compoundV3 will only return one token as reward
+  //   console.log("Reward Token: ", rewardOwed.token);
+  //   console.log("Reward Amount: ", rewardOwed.owed);
+  // }
+
+  function test_harvestWithStrategy1AndSwap() public {
+    uint8 assetDecimals = yieldVault.decimals();
+    uint8 compDecimals = 18;
+    do_deposit(DEPOSIT_AMOUNT, yieldVault, ALICE);
+
+    vm.warp(block.timestamp + (13 * 1 seconds));
+    vm.roll(block.number + 1);
+
+    uint256 balanceBefore = compoundV3.getDepositBalance(address(yieldVault), vault);
+
+    address market = CompoundV3(address(compoundV3)).getMapper().getAddressNestedMapping(
+      compoundV3.providerName(), debtAsset, address(0)
+    );
+    RewardOwed memory rewardOwed = ICompoundV3Rewards(0x1B0e765F6224C21223AeA2af16c1C46E38885a40)
+      .getRewardOwed(market, address(yieldVault));
+
+    bytes memory data = abi.encode(yieldVault);
+    vm.startPrank(HARVESTER);
+    harvestManager.harvest(
+      yieldVault, Strategy.ConvertToCollateral, IHarvestable(address(compoundV3)), swapper, data
+    );
+    vm.stopPrank();
+
+    uint256 expectedRewardsInCompound = rewardOwed.owed / 10;
+    uint256 expectedRewards = expectedRewardsInCompound
+      * oracle.getPriceOf(collateralAsset, debtAsset, assetDecimals) / (10 ** compDecimals); //in collateralAsset
+
+    uint256 rewardsInyieldVault =
+      compoundV3.getDepositBalance(address(yieldVault), vault) - balanceBefore;
+
+    assertEq(rewardsInyieldVault, expectedRewards);
+  }
+
+  function test_harvestWithStrategy2AndSwap() public {
+    uint8 debtDecimals = vault.debtDecimals();
+    uint8 compDecimals = 18;
+
+    do_depositAndBorrow(DEPOSIT_AMOUNT, BORROW_AMOUNT, vault, ALICE);
+
+    vm.warp(block.timestamp + (13 * 1000 seconds));
+    vm.roll(block.number + 1000);
+
+    uint256 borrowBalanceBefore = compoundV3.getBorrowBalance(address(vault), vault);
+
+    bytes memory data = abi.encode(vault);
+    vm.startPrank(HARVESTER);
+    harvestManager.harvest(
+      vault, Strategy.RepayDebt, IHarvestable(address(compoundV3)), swapper, data
+    );
+    vm.stopPrank();
+
+    address market = CompoundV3(address(compoundV3)).getMapper().getAddressNestedMapping(
+      compoundV3.providerName(), collateralAsset, debtAsset
+    );
+    RewardOwed memory rewardOwed = ICompoundV3Rewards(0x1B0e765F6224C21223AeA2af16c1C46E38885a40)
+      .getRewardOwed(market, address(yieldVault));
+
+    uint256 expectedRewardsInCompound = rewardOwed.owed / 10;
+    uint256 expectedRewardsInDebtAsset = expectedRewardsInCompound
+      * oracle.getPriceOf(debtAsset, rewardOwed.token, debtDecimals) / (10 ** compDecimals); //in debtAsset
+
+    uint256 expectedBorrowBalance = 0;
+    uint256 expectedTreasuryBalance = 0;
+
+    if (borrowBalanceBefore >= expectedRewardsInDebtAsset) {
+      expectedBorrowBalance = borrowBalanceBefore - expectedRewardsInDebtAsset;
+      expectedTreasuryBalance = 0;
+    } else {
+      expectedBorrowBalance = 0;
+      expectedTreasuryBalance = expectedRewardsInDebtAsset - borrowBalanceBefore;
+    }
+
+    //assert debt in vault
+    assertEq(compoundV3.getBorrowBalance(address(vault), vault), expectedBorrowBalance);
+    //assert treasury detbAsset balance
+    assertEq(IERC20(debtAsset).balanceOf(TREASURY), expectedTreasuryBalance);
+  }
 }
