59df8e6a066cf61e160ab033f7b9a93344422e01
pedrovalido <pedro.validex@gmail.com>
2023-08-11 13:44:16 +0100

    refactor(protocol): spark with efficiency mode

--- packages/protocol/src/providers/mainnet/Spark.sol
+++ packages/protocol/src/providers/mainnet/Spark.sol
@@ -12,106 +12,23 @@ pragma solidity 0.8.15;
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {IVault} from "../../interfaces/IVault.sol";
 import {ILendingProvider} from "../../interfaces/ILendingProvider.sol";
-import {IV3Pool} from "../../interfaces/aaveV3/IV3Pool.sol";
+import {AaveV3Common, IV3Pool, AaveEModeHelper} from "../AaveV3Common.sol";
 
-contract Spark is ILendingProvider {
+contract Spark is AaveV3Common {
   /**
    * @dev Returns the {IV3Pool} pool to interact with Spark
    */
-  function _getPool() internal pure returns (IV3Pool) {
+  function _getPool() internal pure override returns (IV3Pool) {
     return IV3Pool(0xC13e21B648A5Ee794902342038FF3aDAB66BE987);
+  }
+
+  ///@inheritdoc AaveV3Common
+  function _getAaveEModeHelper() internal pure override returns (AaveEModeHelper) {
+    return AaveEModeHelper(address(0));
   }
 
   /// @inheritdoc ILendingProvider
   function providerName() public pure override returns (string memory) {
     return "Spark";
-  }
-
-  /// @inheritdoc ILendingProvider
-  function approvedOperator(
-    address,
-    address,
-    address
-  )
-    external
-    pure
-    override
-    returns (address operator)
-  {
-    operator = address(_getPool());
-  }
-
-  /// @inheritdoc ILendingProvider
-  function deposit(uint256 amount, IVault vault) external override returns (bool success) {
-    IV3Pool aave = _getPool();
-    address asset = vault.asset();
-    aave.supply(asset, amount, address(vault), 0);
-    aave.setUserUseReserveAsCollateral(asset, true);
-    success = true;
-  }
-
-  /// @inheritdoc ILendingProvider
-  function borrow(uint256 amount, IVault vault) external override returns (bool success) {
-    IV3Pool spark = _getPool();
-    spark.borrow(vault.debtAsset(), amount, 2, 0, address(vault));
-    success = true;
-  }
-
-  /// @inheritdoc ILendingProvider
-  function withdraw(uint256 amount, IVault vault) external override returns (bool success) {
-    IV3Pool spark = _getPool();
-    spark.withdraw(vault.asset(), amount, address(vault));
-    success = true;
-  }
-
-  /// @inheritdoc ILendingProvider
-  function payback(uint256 amount, IVault vault) external override returns (bool success) {
-    IV3Pool spark = _getPool();
-    spark.repay(vault.debtAsset(), amount, 2, address(vault));
-    success = true;
-  }
-
-  /// @inheritdoc ILendingProvider
-  function getDepositRateFor(IVault vault) external view override returns (uint256 rate) {
-    IV3Pool sparkData = _getPool();
-    IV3Pool.ReserveData memory rdata = sparkData.getReserveData(vault.asset());
-    rate = rdata.currentLiquidityRate;
-  }
-
-  /// @inheritdoc ILendingProvider
-  function getBorrowRateFor(IVault vault) external view override returns (uint256 rate) {
-    IV3Pool sparkData = _getPool();
-    IV3Pool.ReserveData memory rdata = sparkData.getReserveData(vault.debtAsset());
-    rate = rdata.currentVariableBorrowRate;
-  }
-
-  /// @inheritdoc ILendingProvider
-  function getDepositBalance(
-    address user,
-    IVault vault
-  )
-    external
-    view
-    override
-    returns (uint256 balance)
-  {
-    IV3Pool sparkData = _getPool();
-    IV3Pool.ReserveData memory rdata = sparkData.getReserveData(vault.asset());
-    balance = IERC20(rdata.aTokenAddress).balanceOf(user);
-  }
-
-  /// @inheritdoc ILendingProvider
-  function getBorrowBalance(
-    address user,
-    IVault vault
-  )
-    external
-    view
-    override
-    returns (uint256 balance)
-  {
-    IV3Pool sparkData = _getPool();
-    IV3Pool.ReserveData memory rdata = sparkData.getReserveData(vault.debtAsset());
-    balance = IERC20(rdata.variableDebtTokenAddress).balanceOf(user);
   }
 }
