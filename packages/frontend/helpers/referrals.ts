f8fc9bbb3e30ef2d39ce78e2a6e4a92ed34cb3da
Federico Erostarbe <erostarbe@gmail.com>
2023-08-22 16:15:16 -0300

    fix(frontend): pass chainvine api key to sdk

--- packages/frontend/helpers/referrals.ts
+++ packages/frontend/helpers/referrals.ts
@@ -5,6 +5,7 @@ import {
 } from '@chainvine/sdk';
 
 const config = {
+  apiKey: process.env.NEXT_PUBLIC_REFERRAL_API_KEY,
   logToConsole: true,
   testMode: true,
 };
@@ -13,7 +14,6 @@ export const campaignId = process.env.NEXT_PUBLIC_REFERRAL_CAMPAIGN_ID ?? '';
 
 export const widgetConfig = {
   ...config,
-  apiKey: process.env.NEXT_PUBLIC_REFERRAL_API_KEY,
 };
 
 const campaign = {
