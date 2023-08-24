0cedd2fedd33f735a5076a02d8c06a8e2031dd2c
Federico Erostarbe <erostarbe@gmail.com>
2023-08-24 11:13:24 -0300

    chore(frontend): pass base url to widget

--- packages/frontend/helpers/referrals.ts
+++ packages/frontend/helpers/referrals.ts
@@ -14,6 +14,7 @@ export const campaignId = process.env.NEXT_PUBLIC_REFERRAL_CAMPAIGN_ID ?? '';
 
 export const widgetConfig = {
   ...config,
+  baseUrl: process.env.NEXT_PUBLIC_REFERRAL_BASE_URL,
 };
 
 const campaign = {
