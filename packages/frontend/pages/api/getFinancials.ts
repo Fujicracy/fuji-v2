8fa52777064b803aa48466e8844345b15afe9d40
Federico Erostarbe <erostarbe@gmail.com>
2023-08-25 18:49:46 -0300

    chore(frontend): api cleanup

--- packages/frontend/pages/api/getFinancials.ts
+++ packages/frontend/pages/api/getFinancials.ts
@@ -5,19 +5,20 @@ import { STATUS } from './helpers/constants';
 import { getFinancialsAndStakingFromDB } from './helpers/db';
 
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
-  const result = await getFinancialsAndStakingFromDB();
-  console.log(result.staking);
-  console.log('GOT RESULT');
-  // Update pools with staking rewards. Might have to improve based on more checks than just symbol
-  result.pools.forEach((pool: LlamaAssetPool) => {
-    const match = result.staking.find((apy) => apy.symbol === pool.symbol);
-    if (match) {
-      console.log('MATCH');
-      pool.stakingApy = match.value;
-    }
-  });
+  try {
+    const result = await getFinancialsAndStakingFromDB();
+    // Update pools with staking rewards. Might have to improve based on more checks than just symbol
+    result.pools.forEach((pool: LlamaAssetPool) => {
+      const match = result.staking.find((apy) => apy.symbol === pool.symbol);
+      if (match) {
+        pool.stakingApy = match.value;
+      }
+    });
 
-  res
-    .status(STATUS.SUCCESS)
-    .json({ pools: result.pools, lendBorrows: result.lendBorrows });
+    res
+      .status(STATUS.SUCCESS)
+      .json({ pools: result.pools, lendBorrows: result.lendBorrows });
+  } catch (error) {
+    res.status(STATUS.ERROR).json({ error });
+  }
 }
