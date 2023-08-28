ca9fe5f5e6894c1f1e09e0db2f16f706a02ec74a
Federico Erostarbe <erostarbe@gmail.com>
2023-08-28 19:16:33 -0300

    chore(frontend): clean constants

--- packages/frontend/pages/api/helpers/constants.ts
+++ packages/frontend/pages/api/helpers/constants.ts
@@ -1,9 +1,6 @@
-export const PORT = Number(process.env.PORT) || 3000;
-export const REDIS_URL = process.env.REDIS_URL ?? '';
 export const DEFILLAMA_PROXY = process.env.DEFILLAMA_PROXY;
 
 export const FIFTEEN_MINUTES = 15 * 60 * 1000;
-export const THREE_DAYS_IN_SECONDS = 3 * 24 * 60 * 60;
 
 export const REFRESH_INTERVAL = FIFTEEN_MINUTES;
 
