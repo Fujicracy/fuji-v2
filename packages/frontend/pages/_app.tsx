5c2ac79ab4ca2c0aab5d967add617ae3f083a656
Boyan Barakov <boyan.barakov@gmail.com>
2023-10-06 13:41:36 +0200

    chore(frontend): remove guild access

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -7,7 +7,6 @@ import { useRouter } from 'next/router';
 import Script from 'next/script';
 import { useEffect, useRef } from 'react';
 
-import { GuildAccess } from '../components/App/GuildAccess';
 import Header from '../components/App/Header/Header';
 import Notification from '../components/App/Notification';
 import DisclaimerModal from '../components/App/Onboarding/DisclaimerModal';
@@ -26,8 +25,6 @@ import { useHistory } from '../store/history.store';
 import { useNavigation } from '../store/navigation.store';
 import { usePositions } from '../store/positions.store';
 import { theme } from '../styles/theme';
-
-const APP_ENV = process.env.NEXT_PUBLIC_APP_ENV;
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -154,7 +151,6 @@ function MyApp({ Component, pageProps }: AppProps) {
               isHistoricalTransaction={isHistoricalTransaction}
             />
           )}
-          {APP_ENV === 'production' && <GuildAccess />}
           <DisclaimerModal />
           <ExploreCarousel />
           <Notification />
